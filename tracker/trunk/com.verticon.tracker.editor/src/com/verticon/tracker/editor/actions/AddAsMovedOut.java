fd0eb8de8ed23d279d69beecc43cf5b9b1cdd8d8
jconlon <jconlon@0cc8e0f8-e01b-0410-8bcf-b9e3f263e42e>
2007-05-11 20:11:06 +0000

    Initial commit

---
+++ tracker/trunk/com.verticon.tracker.editor/src/com/verticon/tracker/editor/actions/AddAsMovedOut.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.verticon.tracker.editor.actions;
+
+import com.verticon.tracker.Event;
+import com.verticon.tracker.MovedOut;
+import com.verticon.tracker.TrackerFactory;
+
+/**
+ * @author jconlon
+ *
+ */
+public class AddAsMovedOut extends AbstractSimpleAddAsEvent {
+
+	/**
+	 * Create a MovedOut event if there is not already one assigned to the tag.
+	 * @return event or null.
+	 * @see com.verticon.tracker.editor.actions.AbstractAddAsEvent#createEvent(com.verticon.tracker.TrackerFactory)
+	 */
+	@Override
+	protected Event createEvent(TrackerFactory trackerFactory, Long tag) {
+		return containsEvent( tag, MovedOut.EVENT_CODE)?
+				null:trackerFactory.createMovedOut();
+	}
+
+}
