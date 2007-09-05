c472e88f9c838c8169ce8955173675e62669cea6
jconlon <jconlon@0cc8e0f8-e01b-0410-8bcf-b9e3f263e42e>
2007-09-05 15:46:09 +0000

    CLOSED - # 23: Add Duplicate Event Timeout preference http://localhost:8000/tracker/ticket/23

--- tracker/trunk/com.verticon.tracker.editor/src/com/verticon/tracker/editor/preferences/PreferenceInitializer.java
+++ tracker/trunk/com.verticon.tracker.editor/src/com/verticon/tracker/editor/preferences/PreferenceInitializer.java
@@ -1,14 +1,15 @@
-package com.verticon.tracker.transaction.publisher.preferences;
+package com.verticon.tracker.editor.preferences;
 
 import org.eclipse.core.runtime.Preferences;
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
-
-import com.verticon.tracker.transaction.publisher.TrackerFileTailPublisherPlugin;
+import com.verticon.tracker.editor.presentation.TrackerReportEditorPlugin;
 
 /**
  * Class used to initialize default preference values.
  */
 public class PreferenceInitializer extends AbstractPreferenceInitializer {
+
+	
 
 	/*
 	 * (non-Javadoc)
@@ -16,12 +17,9 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
 	 * @see org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer#initializeDefaultPreferences()
 	 */
 	public void initializeDefaultPreferences() {
-		Preferences store = TrackerFileTailPublisherPlugin.getDefault().getPluginPreferences();
+		Preferences store = TrackerReportEditorPlugin.getPlugin().getPluginPreferences();
 		
-//		store.setDefault(PreferenceConstants.P_ON_BOOLEAN, false);
-		store.setDefault(PreferenceConstants.P_READ_INTERVAL, 1);
-		
-
+		store.setDefault(PreferenceConstants.P_IGNORE_WINDOW,PreferenceConstants.DEFAULT_IGNORE_WINDOW);
 	}
 
 }
