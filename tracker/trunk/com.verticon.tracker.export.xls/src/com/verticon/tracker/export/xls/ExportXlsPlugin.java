811809e02f5e9ab9bd2c3b1810f42d9992510d17
John Conlon <jconlon@verticon.com>
2007-09-25 20:54:19 +0000

    CLOSED - # 63: Refactor Export actions to use File export Wizards. http://localhost:8000/tracker/ticket/63

--- tracker/trunk/com.verticon.tracker.export.xls/src/com/verticon/tracker/export/xls/ExportXlsPlugin.java
+++ tracker/trunk/com.verticon.tracker.export.xls/src/com/verticon/tracker/export/xls/ExportXlsPlugin.java
@@ -6,18 +6,18 @@ import org.osgi.framework.BundleContext;
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Activator extends AbstractUIPlugin {
+public class ExportXlsPlugin extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "com.verticon.tracker.export.xls";
+	public static final String ID = "com.verticon.tracker.export.xls";
 
 	// The shared instance
-	private static Activator plugin;
+	private static ExportXlsPlugin plugin;
 	
 	/**
 	 * The constructor
 	 */
-	public Activator() {
+	public ExportXlsPlugin() {
 		plugin = this;
 	}
 
@@ -43,7 +43,7 @@ public class Activator extends AbstractUIPlugin {
 	 *
 	 * @return the shared instance
 	 */
-	public static Activator getDefault() {
+	public static ExportXlsPlugin getDefault() {
 		return plugin;
 	}
 
