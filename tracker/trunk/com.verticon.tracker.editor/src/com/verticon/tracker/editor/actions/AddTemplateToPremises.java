2ba2af10d372369b4bd44f0b62d679e2d1688682
John Conlon <jconlon@verticon.com>
2007-09-26 23:29:15 +0000

    CLOSED - # 64: Refactor popup actions on tags files to use Wizards instead of plain Dialogs http://localhost:8000/tracker/ticket/64

--- tracker/trunk/com.verticon.tracker.editor/src/com/verticon/tracker/editor/actions/AddTemplateToPremises.java
+++ tracker/trunk/com.verticon.tracker.editor/src/com/verticon/tracker/editor/actions/AddTemplateToPremises.java
@@ -3,24 +3,25 @@
  */
 package com.verticon.tracker.editor.actions;
 
-import java.io.FileNotFoundException;
-
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchPartSite;
+import org.eclipse.ui.IWorkbenchWindow;
 
-import com.verticon.tracker.Premises;
+import com.verticon.tracker.editor.presentation.AddTemplateToPremisesWizard;
 import com.verticon.tracker.editor.presentation.TrackerEditor;
-import com.verticon.tracker.editor.util.ActionUtils;
-import com.verticon.tracker.editor.util.TagsBean;
-import com.verticon.tracker.editor.util.TemplateAnimalBean;
-import com.verticon.tracker.util.TrackerLog;
 
 /**
- * Action associated with a selection on a Capture *.tags file.  Prompts the user to select a Template
- * to add to the set of animals associated with the AINs in the capture *.tags file.
+ * Action associated with a selection on a Capture *.tags file.
+ * 
+ * Calls Wizard that prompts the user to select a Template to add to the set of animals 
+ * associated with the AINs in the capture *.tags file.
  * 
  * For AINs that are not already assigned to animals an animal will be created.
  * 
@@ -33,11 +34,8 @@ import com.verticon.tracker.util.TrackerLog;
  */
 public class AddTemplateToPremises implements IObjectActionDelegate {
 
-
-	
-	
 	private IWorkbenchPart targetPart;
-	private ISelection selection;
+	private IStructuredSelection selectionOfTagIdResources;
 	
 	/* (non-Javadoc)
 	 * @see org.eclipse.ui.IObjectActionDelegate#setActivePart(org.eclipse.jface.action.IAction, org.eclipse.ui.IWorkbenchPart)
@@ -47,55 +45,33 @@ public class AddTemplateToPremises implements IObjectActionDelegate {
 
 	}
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
-	 */
 	public void run(IAction action) {
-		TrackerEditor editor = ActionUtils.getTrackerEditor(targetPart);
-		Premises premises = ActionUtils.getPremises( editor);
-		if(editor==null){
-			MessageDialog.openError(targetPart.getSite().getShell(),
-					"Failed to add tags",
-					"Could not find an active TrackerEditor ");
-			return;
-		}
+		IWorkbenchPartSite site = targetPart.getSite();
+		IWorkbenchWindow workbenchWindow = site.getWorkbenchWindow();
 		
-		TemplateAnimalBean templateBean = ActionUtils.getTemplateBean( editor,  targetPart);
-		if(templateBean == null){
+		IEditorPart editorPart = workbenchWindow.getActivePage().getActiveEditor();
+		if(editorPart==null|| (!(editorPart instanceof TrackerEditor))){
+			MessageDialog.openError(workbenchWindow.getShell(),
+					"Failed to add template",
+					"The Active Editor is not a TrackerEditor ");
 			return;
 		}
-
-		TagsBean tagsBean = null;
-		try {
-			 tagsBean = ActionUtils.getTagsBean(editor, selection);
-			if(tagsBean==null || tagsBean.getTags().isEmpty()){
-				MessageDialog.openWarning(targetPart.getSite().getShell(),
-							"No unique tags in file",
-							"Found no unique events to import from template "+tagsBean.getName());
-				return;
-			}
-			ActionUtils.addTemplate(premises, tagsBean, templateBean, editor);
-			//Refresh the current viewer
-			editor.getViewer().refresh();
-		} catch (FileNotFoundException e) {
-			TrackerLog.logError("Could not find the tags file.", e);
-			MessageDialog
-			.openError(
-					targetPart.getSite().getShell(),
-					"Failed to add tags",
-					"Failed to add tags from file "
-							+ tagsBean.getName()
-							+ " because: "
-							+ e.getLocalizedMessage());
-		}
+		TrackerEditor editor =(TrackerEditor)editorPart;
+		AddTemplateToPremisesWizard wizard = new AddTemplateToPremisesWizard();
+		wizard.init(workbenchWindow, editor, selectionOfTagIdResources);
+		WizardDialog dialog = new WizardDialog(workbenchWindow.getShell(), wizard);
+		dialog.open();
+		
 	}
 	
 	/* (non-Javadoc)
 	 * @see org.eclipse.ui.IActionDelegate#selectionChanged(org.eclipse.jface.action.IAction, org.eclipse.jface.viewers.ISelection)
 	 */
 	public void selectionChanged(IAction action, ISelection selection) {
-		this.selection = selection;
-		action.setEnabled(!selection.isEmpty());
+		this.selectionOfTagIdResources = 
+			selection instanceof IStructuredSelection
+			? (IStructuredSelection)selection
+			: null;
 	}
 	
 }
