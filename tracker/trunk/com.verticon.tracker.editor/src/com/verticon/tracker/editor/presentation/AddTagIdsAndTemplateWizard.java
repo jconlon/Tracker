9bde22a000a7a07d0eaa669055283876673b83fe
jconlon <jconlon@0cc8e0f8-e01b-0410-8bcf-b9e3f263e42e>
2007-10-02 18:19:28 +0000

    ASSIGNED - # 11: Publisher configuration and operation is too confusing http://localhost:8000/tracker/ticket/11

--- tracker/trunk/com.verticon.tracker.editor/src/com/verticon/tracker/editor/presentation/AddTagIdsAndTemplateWizard.java
+++ tracker/trunk/com.verticon.tracker.editor/src/com/verticon/tracker/editor/presentation/AddTagIdsAndTemplateWizard.java
@@ -3,51 +3,40 @@
  */
 package com.verticon.tracker.editor.presentation;
 
-import java.util.Collection;
+import java.io.FileNotFoundException;
 
-import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.IWorkbenchWindow;
 
-import com.verticon.tracker.Animal;
+import com.verticon.tracker.Premises;
 import com.verticon.tracker.editor.util.ActionUtils;
+import com.verticon.tracker.editor.util.TagsBean;
 import com.verticon.tracker.editor.util.AnimalTemplateBean;
 
 /**
- * Wizard for adding a template animal document to one or more animals.
+ * Wizard for adding a template animal document to a Premises
+ * based on a selection of TagID files (*.tags).
  * 
  * Pages: 1. Select the Animal document to use as a template
  * 
  * @author jconlon
  * 
  */
-public class AddTemplateToAnimalsWizard extends Wizard {
+public class AddTagIdsAndTemplateWizard extends Wizard {
 
-	private static final String MODIFY_WIZARD_TITLE = "Add Template To Animals";
-	public static final String MODIFY_EVENTS_WIZARD = "ModifyEventsWizard";
-	private Collection<Animal> selectedAnimals;
+	private static final String MODIFY_WIZARD_TITLE = "Add Template To Premises";
+	private IStructuredSelection selectionOfTagIdResources;
 	private TrackerEditor editor;
 	@SuppressWarnings("unused")
 	private IWorkbenchWindow workbenchWindow;
 	private SelectAnimalDocumentWizardPage selectAnimalDocumentWizardPage;
 
-	public AddTemplateToAnimalsWizard() {
-		IDialogSettings trackerSettings = TrackerReportEditorPlugin.getPlugin()
-				.getDialogSettings();
-		IDialogSettings wizardSettings = trackerSettings
-				.getSection(MODIFY_EVENTS_WIZARD);
-		if (wizardSettings == null) {
-			wizardSettings = trackerSettings
-					.addNewSection(MODIFY_EVENTS_WIZARD);
-		}
-		setDialogSettings(wizardSettings);
-	}
 
 	public void init(IWorkbenchWindow workbenchWindow, TrackerEditor editor,
 			IStructuredSelection selection) {
 		this.workbenchWindow = workbenchWindow;
-		selectedAnimals = ActionUtils.getSelectedAnimals(selection);
+		this.selectionOfTagIdResources = selection;
 		this.editor = editor;
 	}
 
@@ -65,10 +54,26 @@ public class AddTemplateToAnimalsWizard extends Wizard {
 		addPage(selectAnimalDocumentWizardPage);
 		selectAnimalDocumentWizardPage.init(editor);
 	}
+	
 
 	@Override
 	public boolean performFinish() {
-		ActionUtils.addTemplateToAnimals(selectedAnimals, selectAnimalDocumentWizardPage
+
+		Premises premises = ActionUtils.getPremises( editor);
+		TagsBean tagsBean = null;
+
+		try {
+			tagsBean = ActionUtils.getTagsBean(editor, selectionOfTagIdResources);
+		} catch (FileNotFoundException e) {
+			selectAnimalDocumentWizardPage.setErrorMessage(e.getMessage());
+			return false;
+		}
+		if(tagsBean==null || tagsBean.getTags().isEmpty()){
+			selectAnimalDocumentWizardPage.setErrorMessage(
+					"Found no unique events to import from template "+tagsBean.getName());
+			return false;
+		}
+		ActionUtils.addTagsAndTemplate(premises, tagsBean, selectAnimalDocumentWizardPage
 				.getTemplateBean(), editor);
 		// Refresh the current viewer
 		editor.getViewer().refresh();
