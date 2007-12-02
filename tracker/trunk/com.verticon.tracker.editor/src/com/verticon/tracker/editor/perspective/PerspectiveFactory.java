60c1076ce2ac287406e006abfb1ef204ee715299
jconlon <jconlon@0cc8e0f8-e01b-0410-8bcf-b9e3f263e42e>
2007-12-02 19:39:15 +0000

    CLOSED - # 10: Add a Tracker perspective http://localhost:8000/tracker/ticket/10

---
+++ tracker/trunk/com.verticon.tracker.editor/src/com/verticon/tracker/editor/perspective/PerspectiveFactory.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package com.verticon.tracker.editor.perspective;
+
+import org.eclipse.ui.IFolderLayout;
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+/**
+ * @author jconlon
+ *
+ */
+public class PerspectiveFactory implements IPerspectiveFactory {
+
+	private static final String ORG_ECLIPSE_UI_CONSOLE_CONSOLE_VIEW = "org.eclipse.ui.console.ConsoleView";
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IPerspectiveFactory#createInitialLayout(org.eclipse.ui.IPageLayout)
+	 */
+	public void createInitialLayout(IPageLayout layout) {
+String editorArea = layout.getEditorArea();
+		
+		
+		//Left folder
+		IFolderLayout left =
+			layout.createFolder(
+					"left", 
+					IPageLayout.LEFT, 
+					0.25f, 
+					editorArea);
+		//With an outline
+		left.addView(IPageLayout.ID_RES_NAV);
+		
+		//Bottom left is the navigator
+		layout.addView(
+				IPageLayout.ID_OUTLINE, 
+				IPageLayout.BOTTOM, 
+				0.66f, 
+				"left");
+		
+		//A bottom folder
+		IFolderLayout bottom =
+			layout.createFolder(
+					"bottom", 
+					IPageLayout.BOTTOM, 
+					0.66f, 
+					editorArea);
+		bottom.addView(IPageLayout.ID_PROP_SHEET);
+//		bottom.addView(PUBLISHER_VIEW);
+//		bottom.addView(OCL_VIEW);
+		bottom.addView(ORG_ECLIPSE_UI_CONSOLE_CONSOLE_VIEW);
+		bottom.addView(IPageLayout.ID_PROBLEM_VIEW);
+		bottom.addView(IPageLayout.ID_TASK_LIST);
+		
+
+	}
+
+}
