008d7d83882e87975de112cc673de97c52328db7
John Conlon <jconlon@verticon.com>
2008-02-15 16:59:22 +0000

    CLOSED - # 147: Simplify Model Element menu http://localhost:8000/tracker/ticket/147

--- tracker/trunk/com.verticon.tracker.editor/src/com/verticon/tracker/editor/preferences/internal/PreferenceInitializer.java
+++ tracker/trunk/com.verticon.tracker.editor/src/com/verticon/tracker/editor/preferences/internal/PreferenceInitializer.java
@@ -1,7 +1,9 @@
-package com.verticon.tracker.editor.preferences;
+package com.verticon.tracker.editor.preferences.internal;
 
 import org.eclipse.core.runtime.Preferences;
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+
+import com.verticon.tracker.editor.preferences.PreferenceConstants;
 import com.verticon.tracker.editor.presentation.TrackerReportEditorPlugin;
 
 /**
@@ -21,6 +23,7 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
 		
 		store.setDefault(PreferenceConstants.P_IGNORE_WINDOW,PreferenceConstants.DEFAULT_IGNORE_WINDOW);
 		store.setDefault(PreferenceConstants.P_VALIDATE_BEFORE_EXPORT,PreferenceConstants.DEFAULT_VALIDATE_BEFORE_EXPORT);
+		store.setDefault(PreferenceConstants.P_USE_SUBMENUS,PreferenceConstants.DEFAULT_USE_SUBMENUS);
 	}
 
 }
