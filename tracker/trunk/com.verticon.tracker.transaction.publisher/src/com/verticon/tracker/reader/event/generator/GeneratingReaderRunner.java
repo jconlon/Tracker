cf6045ec8064b5d72e99a5f4dacd2b1b1dda3b3b
John Conlon <jconlon@verticon.com>
2008-04-09 16:10:30 +0000

    Typo on log statement

--- tracker/trunk/com.verticon.tracker.transaction.publisher/src/com/verticon/tracker/reader/event/generator/GeneratingReaderRunner.java
+++ tracker/trunk/com.verticon.tracker.transaction.publisher/src/com/verticon/tracker/reader/event/generator/GeneratingReaderRunner.java
@@ -97,7 +97,7 @@ public class GeneratingReaderRunner implements Runnable {
 	private void sendARandomTag(){
 		int index = (int) (Math.random() * cache.size());
 		Long tag = cache.get(index);
-		logger.info("{} sending {} which is #{} out of {} cached tags.",
+		logger.info("Sending {} which is #{} out of {} cached tags.",
 				new Object[]{tag, index, cache.size()});
 		tagIDPublisher.publish(tag);
 	}
