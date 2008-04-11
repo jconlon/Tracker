9eff6b6652134578462244e05c08d00c5b4d93b3
John Conlon <jconlon@verticon.com>
2008-04-11 00:28:20 +0000

    CLOSED - # 189: Fair Editor Events table is not updated when the underlying Premises document changes. http://localhost:8000/tracker/ticket/189

--- tracker/trunk/com.verticon.tracker.editor/src/com/verticon/tracker/editor/presentation/EventsTableViewerNotifier.java
+++ tracker/trunk/com.verticon.tracker.editor/src/com/verticon/tracker/editor/presentation/EventsTableViewerNotifier.java
@@ -7,25 +7,25 @@ import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.util.EContentAdapter;
-import org.eclipse.emf.edit.ui.provider.NotifyChangedToViewerRefresh;
 import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.widgets.Display;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.verticon.tracker.TrackerPackage;
 
 /**
- * Sends Event notifications to a tableViewer. 
+ * Sends Event notifications to a tableViewer.
+ * 
  * @author jconlon
- *
+ * 
  */
-public class EventsTableViewerNotifier extends EContentAdapter{
+public class EventsTableViewerNotifier extends EContentAdapter {
 
 	/**
 	 * slf4j Logger
 	 */
-	protected final Logger logger = LoggerFactory
-		.getLogger(this.getClass());
+	protected final Logger logger = LoggerFactory.getLogger(this.getClass());
 	/**
 	 * Cache of the targetResource
 	 */
@@ -34,57 +34,64 @@ public class EventsTableViewerNotifier extends EContentAdapter{
 	 * EventsTable to notify
 	 */
 	private final TableViewer eventsTableViewer;
-	
+
 	public EventsTableViewerNotifier(TableViewer eventsTableViewer) {
 		super();
-		this.eventsTableViewer=eventsTableViewer;
-		
+		this.eventsTableViewer = eventsTableViewer;
+
 	}
-	
-	
+
 	public void setResource(Resource resource) {
 		logger.debug("Setting resource {}", resource.getURI());
-		this.activeResource=resource.getResourceSet();
+		this.activeResource = resource.getResourceSet();
 		super.setTarget(activeResource);
 	}
 
-	public void unset(){
-		if(activeResource !=null){
+	public void unset() {
+		if (activeResource != null) {
 			super.unsetTarget(activeResource);
 		}
 	}
 
 	@Override
-	public void notifyChanged(Notification notification){
-		super.notifyChanged(notification);//needed to walk the entire model
-		if(eventsTableViewer == null || 
-				eventsTableViewer.getControl() == null ||
-				eventsTableViewer.getControl().isDisposed()){
+	public void notifyChanged(Notification notification) {
+		super.notifyChanged(notification);// needed to walk the entire model
+		if (eventsTableViewer == null || eventsTableViewer.getControl() == null
+				|| eventsTableViewer.getControl().isDisposed()) {
 			return;
 		}
-		
 
-		if (notification.getFeature() == TrackerPackage.eINSTANCE.getTag_Events() )
-	    {
-			logger.debug("Calling NotifiedChangedToViewerRefresh for Tag_Events, from a {}",notification.getNotifier().getClass());
-	        NotifyChangedToViewerRefresh.handleNotifyChanged(
-	          eventsTableViewer,
-	          notification.getNotifier(),
-	          notification.getEventType(),
-	          notification.getFeature(),
-	          notification.getOldValue(),
-	          notification.getNewValue(),
-	          notification.getPosition());
-	        
-	     }
-		 else if (notification.getFeature() == TrackerPackage.eINSTANCE.getTag_Id()	)
-	  	    {
-			 logger.debug("Calling Refreshing eventsTable for Tag_ID, from a {}",notification.getNotifier().getClass());
-		        
-			   //Instead of figuring out the specifics just refresh
-			    eventsTableViewer.refresh();  	        
-	  	      }
+		if (notification.getFeature() == TrackerPackage.eINSTANCE
+				.getTag_Events()) {
+			logger.debug("Refreshing Events table for Tag_Events");
+
+			refreshViewer(eventsTableViewer);
+
+		} else if (notification.getFeature() == TrackerPackage.eINSTANCE
+				.getTag_Id()) {
+			logger.debug( "Refreshing eventsTable for Tag_ID, from a {}");
+			refreshViewer(eventsTableViewer); 
+		}
 
 	}
-}
 
+	/**
+	 * @param tableViewer 
+	 * 
+	 */
+	protected void refreshViewer(final TableViewer tableViewer) {
+		Display d = tableViewer.getControl().getDisplay();
+		if (d != Display.getCurrent()) {
+			d.asyncExec(new Runnable() {
+				public void run() {
+					if (tableViewer.getControl() != null
+							&& !tableViewer.getControl().isDisposed()) {
+						tableViewer.refresh();
+					}
+				}
+			});
+		} else {
+			tableViewer.refresh();
+		}
+	}
+}
