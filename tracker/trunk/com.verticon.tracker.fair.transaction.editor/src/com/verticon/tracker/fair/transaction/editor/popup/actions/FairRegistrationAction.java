1059dd097f3616c8230a74506d6d19eb5ea00745
jconlon <jconlon@0cc8e0f8-e01b-0410-8bcf-b9e3f263e42e>
2008-05-23 17:37:31 +0000

    CLOSED - # 221: Create a popup wizard dialog for adding a new Exhibit to the fair model http://localhost:8000/tracker/ticket/221

---
+++ tracker/trunk/com.verticon.tracker.fair.transaction.editor/src/com/verticon/tracker/fair/transaction/editor/popup/actions/FairRegistrationAction.java
@@ -0,0 +1,78 @@
+package com.verticon.tracker.fair.transaction.editor.popup.actions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.ui.IActionDelegate;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchPartSite;
+import org.eclipse.ui.IWorkbenchWindow;
+
+import com.verticon.tracker.editor.presentation.IQueryDataSetProvider;
+
+public class FairRegistrationAction implements IObjectActionDelegate {
+
+	private static final String FAILED_TO_ADD_EXHIBIT = "Failed to register an animal";
+	
+	private IWorkbenchWindow window;
+	private IStructuredSelection selection;
+	private IWorkbenchPart targetPart;
+	
+	/**
+	 * Constructor for Action1.
+	 */
+	public FairRegistrationAction() {
+		super();
+	}
+	
+	public void init(IWorkbenchWindow window){
+		this.window=window;
+	}
+
+	/**
+	 * @see IObjectActionDelegate#setActivePart(IAction, IWorkbenchPart)
+	 */
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+		this.targetPart=targetPart;
+	}
+
+	/**
+	 * @see IActionDelegate#run(IAction)
+	 */
+	public void run(IAction action) {
+		IWorkbenchPartSite site = targetPart.getSite();
+		IWorkbenchWindow workbenchWindow = site.getWorkbenchWindow();
+		
+		IEditorPart editorPart = workbenchWindow.getActivePage().getActiveEditor();
+
+		
+		IQueryDataSetProvider queryDataSetProvider = (IQueryDataSetProvider)editorPart.getAdapter(IQueryDataSetProvider.class);
+		if(queryDataSetProvider==null){
+			MessageDialog.openError(targetPart.getSite().getShell(),
+					FAILED_TO_ADD_EXHIBIT, "The Active Editor does not support a IQueryDataSetProvider");
+			return;
+		}
+		FairRegistrationWizard wizard = new FairRegistrationWizard();
+		wizard.init(workbenchWindow, queryDataSetProvider.getEditingDomain(), 
+				selection);
+		WizardDialog dialog = new WizardDialog(workbenchWindow.getShell(), wizard);
+		dialog.open();
+		
+	}
+
+	/**
+	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
+	 */
+	public void selectionChanged(IAction action, ISelection selection) {
+		
+		this.selection = 
+			selection instanceof IStructuredSelection
+			? (IStructuredSelection)selection
+			: null;
+	}
+
+}
---
+++ tracker/trunk/com.verticon.tracker.fair.transaction.editor/src/com/verticon/tracker/fair/transaction/editor/popup/actions/UpdateStrategies.java
@@ -0,0 +1,82 @@
+package com.verticon.tracker.fair.transaction.editor.popup.actions;
+
+import org.eclipse.core.databinding.UpdateValueStrategy;
+import org.eclipse.core.databinding.observable.IObserving;
+import org.eclipse.core.databinding.observable.value.IObservableValue;
+import org.eclipse.core.databinding.validation.IValidator;
+import org.eclipse.core.databinding.validation.ValidationStatus;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.ecore.EStructuralFeature;
+
+import com.verticon.tracker.fair.FairPackage;
+import com.verticon.tracker.util.CheckISO7064Mod37_36;
+
+public class UpdateStrategies {
+
+	/**
+	 * The singleton instance of the factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	static UpdateStrategies INSTANCE = new UpdateStrategies();
+
+	
+	protected UpdateValueStrategy getTargetToModelStrategy(EStructuralFeature eStructuralFeature){
+		if(FairPackage.eINSTANCE.getPerson_Pin().equals(eStructuralFeature)){
+			return new CustomUpdateValueStrategy();
+		}
+		return null;
+	}
+	
+	protected UpdateValueStrategy getModelToTargetStrategy(EStructuralFeature eStructuralFeature){
+		
+		return null;
+	}
+	
+	private static final class CustomUpdateValueStrategy extends UpdateValueStrategy {
+
+		@Override
+		public IStatus validateBeforeSet(Object value) {
+			return new MyPinValidator().validate(value);
+		}
+
+		@Override
+		protected IStatus doSet(IObservableValue observableValue, Object value) {
+			IStatus result = super.doSet(observableValue, value);
+			if (result.isOK()) {
+				Object changed = observableValue;
+				if (changed instanceof IObserving) {
+					changed = ((IObserving) changed).getObserved();
+				}
+				
+			}
+			return result;
+		}
+	}
+	
+	private static class MyPinValidator implements IValidator{
+		public IStatus validate(Object value) {
+			String initialValue = (String)value;
+			if(initialValue.length()!=7){
+				return ValidationStatus.error("Premises Id must contain 7 digits: " + initialValue+
+					", to delete enter 7 spaces.");
+			}
+			try {
+				if(CheckISO7064Mod37_36.verify(initialValue)){
+					return ValidationStatus.ok();
+				}
+			} catch (NumberFormatException e) {
+				return ValidationStatus.error("NumberFormat "+e);
+			}
+			
+			return ValidationStatus.error(initialValue+" " +
+					"is a not legal Premises Id. To delete enter 7 spaces.");
+			
+		}
+		
+	}
+	
+	
+
+}
