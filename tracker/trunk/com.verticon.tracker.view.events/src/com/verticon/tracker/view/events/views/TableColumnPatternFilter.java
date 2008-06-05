285b602ee973b8dcb116063a66d52b277ba84b73
jconlon <jconlon@0cc8e0f8-e01b-0410-8bcf-b9e3f263e42e>
2008-06-05 15:03:55 +0000

    CLOSED - # 230: Create a Master Detail View to show Tracker Events http://localhost:8000/tracker/ticket/230

---
+++ tracker/trunk/com.verticon.tracker.view.events/src/com/verticon/tracker/view/events/views/TableColumnPatternFilter.java
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Trevor S. Kaufman.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Trevor S. Kaufman - initial API and implementation
+ ******************************************************************************/
+package com.verticon.tracker.view.events.views;
+
+import org.eclipse.jface.viewers.IBaseLabelProvider;
+import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerFilter;
+
+public class TableColumnPatternFilter extends ViewerFilter {
+
+	private String patternString = "";
+	private int columnIndex;
+
+	@Override
+	public boolean select(Viewer viewer, Object parentElement, Object element) {
+		if (patternString == null) {
+			return true;
+		}
+		
+		String text = null;
+		IBaseLabelProvider baseLabelProvider = ((TableViewer)viewer).getLabelProvider();
+		if (baseLabelProvider instanceof ITableLabelProvider) {
+			ITableLabelProvider tableLabelProvider = (ITableLabelProvider)baseLabelProvider;
+			text = tableLabelProvider.getColumnText(element, columnIndex);
+		} else if (baseLabelProvider instanceof ILabelProvider) {
+			ILabelProvider labelProvider = (ILabelProvider)baseLabelProvider;
+			text = labelProvider.getText(element);
+		} else {
+			text = element.toString();
+		}
+		
+		if (text == null) return false;
+		
+		return text.startsWith(patternString);
+	}
+
+	public void setPattern(String text) {
+		patternString = text;
+	}
+
+	public void setColumnIndex(int columnIndex) {
+		this.columnIndex = columnIndex;
+	}
+
+	public int getColumnIndex() {
+		return columnIndex;
+	}
+
+}
