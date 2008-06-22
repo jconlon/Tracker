19b7cefaf4d2fb9fdedbb31c1caf553133f3de84
jconlon <jconlon@0cc8e0f8-e01b-0410-8bcf-b9e3f263e42e>
2008-06-22 15:27:55 +0000

    CLOSED - # 240: Modify Tree View of Fair to Consolidate References http://localhost:8000/tracker/ticket/240

--- tracker/trunk/com.verticon.tracker.fair.edit/src/com/verticon/tracker/fair/edit/provider/FairItemProvider.java
+++ tracker/trunk/com.verticon.tracker.fair.edit/src/com/verticon/tracker/fair/edit/provider/FairItemProvider.java
@@ -6,21 +6,23 @@
 package com.verticon.tracker.fair.edit.provider;
 
 
-import com.verticon.tracker.fair.Fair;
-import com.verticon.tracker.fair.FairFactory;
-import com.verticon.tracker.fair.FairPackage;
-
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.common.command.CommandWrapper;
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
-
 import org.eclipse.emf.common.util.ResourceLocator;
-
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EStructuralFeature;
-
+import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
+import org.eclipse.emf.edit.provider.IDisposable;
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
 import org.eclipse.emf.edit.provider.IItemLabelProvider;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
@@ -30,6 +32,10 @@ import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
 import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
 import org.eclipse.emf.edit.provider.ItemProviderAdapter;
 import org.eclipse.emf.edit.provider.ViewerNotification;
+
+import com.verticon.tracker.fair.Fair;
+import com.verticon.tracker.fair.FairFactory;
+import com.verticon.tracker.fair.FairPackage;
 
 /**
  * This is the item provider adapter for a {@link com.verticon.tracker.fair.Fair} object.
@@ -60,6 +66,42 @@ public class FairItemProvider
 	 */
 	public FairItemProvider(AdapterFactory adapterFactory) {
 		super(adapterFactory);
+	}
+	
+	/**
+	 * Holder for customChildren for the Fair.
+	 * Ticket 240
+	 */
+	protected List<TransientFairItemProvider> children = null;
+	
+	@Override
+	public Collection<?> getChildren(Object object) {
+		if(children==null){
+			Fair fair = (Fair)object;
+			children = new ArrayList<TransientFairItemProvider>();
+			children.add(
+					new YouthClubsItemProvider(adapterFactory, fair)
+			);
+			children.add(
+					new DivisionsItemProvider(adapterFactory, fair)
+			);
+			children.add(
+					new PeopleItemProvider(adapterFactory, fair)
+			);
+		}
+		return children;
+	}
+	
+	public Object getYouthClubs() {
+		return children.get(0);
+	}
+	
+	public Object getDivisions() {
+		return children.get(1);
+	}
+	
+	public Object getPeople() {
+		return children.get(2);
 	}
 
 	/**
@@ -184,8 +226,12 @@ public class FairItemProvider
 	 * This handles model notifications by calling {@link #updateChildren} to update any cached
 	 * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
 	 * <!-- begin-user-doc -->
+	 * Modified for Ticket 240 
+	 * See EMF Modeling book 14.2.3
+	 * Do not need to send notifications for YouthClubs, the hand implemented 
+	 * YouthClubsItemProvider does it now.
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * @generated NOT
 	 */
 	@Override
 	public void notifyChanged(Notification notification) {
@@ -195,11 +241,11 @@ public class FairItemProvider
 			case FairPackage.FAIR__NAME:
 				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
 				return;
-			case FairPackage.FAIR__YOUTH_CLUBS:
-			case FairPackage.FAIR__DIVISIONS:
-			case FairPackage.FAIR__PEOPLE:
-				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), true, false));
-				return;
+//			case FairPackage.FAIR__YOUTH_CLUBS:
+//			case FairPackage.FAIR__DIVISIONS:
+//			case FairPackage.FAIR__PEOPLE:
+//				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), true, false));
+//				return;
 		}
 		super.notifyChanged(notification);
 	}
@@ -208,27 +254,29 @@ public class FairItemProvider
 	 * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children
 	 * that can be created under this object.
 	 * <!-- begin-user-doc -->
+	 * Modified for Ticket 240 
+	 * See EMF Modeling book 14.2.3
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * @generated NOT
 	 */
 	@Override
 	protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
 		super.collectNewChildDescriptors(newChildDescriptors, object);
+//
+//		newChildDescriptors.add
+//			(createChildParameter
+//				(FairPackage.Literals.FAIR__YOUTH_CLUBS,
+//				 FairFactory.eINSTANCE.createYouthClub()));
 
-		newChildDescriptors.add
-			(createChildParameter
-				(FairPackage.Literals.FAIR__YOUTH_CLUBS,
-				 FairFactory.eINSTANCE.createYouthClub()));
+//		newChildDescriptors.add
+//			(createChildParameter
+//				(FairPackage.Literals.FAIR__DIVISIONS,
+//				 FairFactory.eINSTANCE.createDivision()));
 
-		newChildDescriptors.add
-			(createChildParameter
-				(FairPackage.Literals.FAIR__DIVISIONS,
-				 FairFactory.eINSTANCE.createDivision()));
-
-		newChildDescriptors.add
-			(createChildParameter
-				(FairPackage.Literals.FAIR__PEOPLE,
-				 FairFactory.eINSTANCE.createPerson()));
+//		newChildDescriptors.add
+//			(createChildParameter
+//				(FairPackage.Literals.FAIR__PEOPLE,
+//				 FairFactory.eINSTANCE.createPerson()));
 
 		newChildDescriptors.add
 			(createChildParameter
@@ -246,5 +294,77 @@ public class FairItemProvider
 	public ResourceLocator getResourceLocator() {
 		return FairEditPlugin.INSTANCE;
 	}
+	
+	 public void dispose() // 14.2.3
+	  {
+	    super.dispose();
+	    if (children != null)
+	    {
+	      for (Iterator<?> iter = children.iterator(); iter.hasNext(); )
+	      {
+	        ((IDisposable)iter.next()).dispose();
+	      }
+	    }
+	  }
 
+	 /**
+	  * Modified for Ticket 240 
+	  *  See EMF Modeling book 14.2.3
+	  */
+	 @SuppressWarnings("deprecation")
+	 @Override
+	 protected Command createRemoveCommand(EditingDomain domain, EObject owner,
+			 EReference feature, Collection<?> collection) 
+	 {
+		 return createWrappedCommand(super.createRemoveCommand(domain, owner, feature, collection), owner, feature);
+	 }
+
+	 /**
+	  * Modified for Ticket 240 
+	  * See EMF Modeling book 14.2.3
+	  */
+	 @SuppressWarnings("deprecation")
+	 @Override
+	protected Command createAddCommand(EditingDomain domain, EObject owner,
+			 EReference feature, Collection<?> collection, int index) 
+	 {
+
+		 return createWrappedCommand(super.createAddCommand(
+				 domain, owner, feature, collection, index), owner, feature);
+	 }
+
+	 /**
+	  * Modified for Ticket 240 
+	  * See EMF Modeling book 14.2.3
+	  * @param command
+	  * @param owner
+	  * @param feature
+	  * @return
+	  */
+	 protected Command createWrappedCommand(Command command, 
+			 final EObject owner, final EReference feature) {
+		 if (feature == FairPackage.eINSTANCE.getFair_YouthClubs() 
+				 || feature == FairPackage.eINSTANCE.getFair_Divisions()
+				 || feature == FairPackage.eINSTANCE.getFair_People()){
+			 return  new CommandWrapper(command){
+				 public Collection<?> getAffectedObjects(){
+					 Collection<?> affected = super.getAffectedObjects();
+					 if (affected.contains(owner)){
+						 if(feature == FairPackage.eINSTANCE.getFair_YouthClubs() ){
+							 affected = Collections.singleton(getYouthClubs());
+						 }else if(feature == FairPackage.eINSTANCE.getFair_Divisions()){
+							 affected = Collections.singleton(getDivisions());
+						 }else {
+							 affected = Collections.singleton(getPeople());
+						 }
+					 }
+					 return affected;
+				 }
+			 };
+		 }else
+			 return command;
+	 }
 }
+
+
+
