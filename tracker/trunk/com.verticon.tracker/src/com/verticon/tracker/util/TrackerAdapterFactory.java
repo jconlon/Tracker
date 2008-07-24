1f3251d86e14c3a9b09ff322c5e1116cea0fc502
John Conlon <jconlon@verticon.com>
2008-07-24 19:45:54 +0000

    ASSIGNED - # 253: Add a type attribute to animal http://localhost:8000/tracker/ticket/253

--- tracker/trunk/com.verticon.tracker/src/com/verticon/tracker/util/TrackerAdapterFactory.java
+++ tracker/trunk/com.verticon.tracker/src/com/verticon/tracker/util/TrackerAdapterFactory.java
@@ -112,7 +112,7 @@ public class TrackerAdapterFactory extends AdapterFactoryImpl {
 	}
 
 	/**
-	 * The switch the delegates to the <code>createXXX</code> methods.
+	 * The switch that delegates to the <code>createXXX</code> methods.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
 	 * @generated
