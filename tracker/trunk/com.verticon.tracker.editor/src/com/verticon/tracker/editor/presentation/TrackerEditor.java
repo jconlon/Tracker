01677b528c2dd8da5e9b816015cea59d03c02378
jconlon <jconlon@0cc8e0f8-e01b-0410-8bcf-b9e3f263e42e>
2008-10-13 19:09:47 +0000

    CLOSED - # 264: Add Visual Identity Attribute to Animal http://localhost:8000/tracker/ticket/264

--- tracker/trunk/com.verticon.tracker.editor/src/com/verticon/tracker/editor/presentation/TrackerEditor.java
+++ tracker/trunk/com.verticon.tracker.editor/src/com/verticon/tracker/editor/presentation/TrackerEditor.java
@@ -77,6 +77,7 @@ import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.action.Separator;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.dialogs.ProgressMonitorDialog;
+import org.eclipse.jface.viewers.ColumnWeightData;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.ISelectionProvider;
@@ -85,6 +86,7 @@ import org.eclipse.jface.viewers.ListViewer;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.viewers.StructuredViewer;
+import org.eclipse.jface.viewers.TableLayout;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.jface.viewers.Viewer;
@@ -98,6 +100,8 @@ import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.Tree;
 import org.eclipse.swt.widgets.TreeColumn;
 import org.eclipse.ui.IActionBars;
@@ -131,7 +135,8 @@ import com.verticon.tracker.editor.validation.LiveValidationContentAdapter;
 import com.verticon.tracker.emf.edit.ui.provider.WorkaroundAdapterFactoryLabelProvider;
 
 /**
- * This is an example of a Tracker model editor. <!-- begin-user-doc --> This
+ * This is an example of a Tracker model editor.
+ * <!-- begin-user-doc --> This
  * editor differs from the generated EMF implementation in the following ways:
  * <ul>
  * <li>offers import and export facilities</li>
