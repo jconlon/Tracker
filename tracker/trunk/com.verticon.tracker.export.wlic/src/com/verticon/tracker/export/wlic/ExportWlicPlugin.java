5d63b0aeef7808636e4e2b4bbc449e6a97770b3b
John Conlon <jconlon@verticon.com>
2008-11-25 01:34:18 +0000

    CLOSED - # 288: Replace log4j logging implementation with logback http://localhost:8000/tracker/ticket/288

--- tracker/trunk/com.verticon.tracker.export.wlic/src/com/verticon/tracker/export/wlic/ExportWlicPlugin.java
+++ tracker/trunk/com.verticon.tracker.export.wlic/src/com/verticon/tracker/export/wlic/ExportWlicPlugin.java
@@ -3,6 +3,10 @@ package com.verticon.tracker.export.wlic;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -16,6 +20,20 @@ public class ExportWlicPlugin extends AbstractUIPlugin {
 	private static ExportWlicPlugin plugin;
 	
 	/**
+	 * slf4j Logger
+	 */
+	private final static Logger logger = LoggerFactory.getLogger(ExportWlicPlugin.class);
+
+	/**
+	 * slf4j Marker to keep track of bundle
+	 */
+	public static final Marker bundleMarker = createBundleMarker();
+	private static final Marker createBundleMarker() {
+		Marker bundleMarker = MarkerFactory.getMarker(ID);
+		bundleMarker.add(MarkerFactory.getMarker("IS_MARKER"));
+		return bundleMarker;
+	}
+	/**
 	 * The constructor
 	 */
 	public ExportWlicPlugin() {
@@ -26,17 +44,21 @@ public class ExportWlicPlugin extends AbstractUIPlugin {
 	 * (non-Javadoc)
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
 	 */
+	@Override
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
+		logger.debug(bundleMarker,"Started Bundle");
 	}
 
 	/*
 	 * (non-Javadoc)
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
 	 */
+	@Override
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
+		logger.debug(bundleMarker,"Stopped Bundle");
 	}
 
 	/**
