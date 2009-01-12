703ae9a932baa5fdc6d8acee1edb53c81a6ac24f
jconlon <jconlon@0cc8e0f8-e01b-0410-8bcf-b9e3f263e42e>
2009-01-12 20:24:24 +0000

    NEW - # 299: Move all apache poi dependencies from the com.verticon.tracker.fair feature http://localhost:8000/tracker/ticket/299

---
+++ tracker/trunk/com.verticon.tracker.fair.poi/src/com/verticon/tracker/fair/poi/importxls/Procreator.java
@@ -0,0 +1,20 @@
+package com.verticon.tracker.fair.poi.importxls;
+
+import java.util.List;
+
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.eclipse.emf.common.command.CompoundCommand;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.edit.domain.EditingDomain;
+
+import com.verticon.tracker.fair.Fair;
+
+public interface Procreator  {
+	
+	void process(Fair fair, HSSFRow row, 
+			List<ColumnMapper> listColumnMapper, EObject parent, boolean newParent, 
+			EditingDomain editingDomain, CompoundCommand compoundCommand) throws MissingCriticalDataException;
+	void dispose();
+	
+	String getStatus();
+}
