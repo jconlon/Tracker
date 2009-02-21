db9928a63122af03718e6ec20fa7cf22487382f7
jconlon <jconlon@0cc8e0f8-e01b-0410-8bcf-b9e3f263e42e>
2009-02-21 21:20:51 +0000

    CLOSED - # 324: Add Cheatsheet to create an example reader http://localhost:8000/tracker/ticket/324

---
+++ tracker/trunk/com.verticon.tracker.reader/src/com/verticon/tracker/reader/handlers/AddTagIDReaderHandler.java
@@ -0,0 +1,56 @@
+package com.verticon.tracker.reader.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.verticon.tracker.reader.views.ReaderViewPart;
+import com.verticon.tracker.reader.wizards.AddEventReaderWizard;
+
+public class AddTagIDReaderHandler extends AbstractHandler{
+
+	/**
+	 * slf4j Logger
+	 */
+	private final Logger logger = LoggerFactory
+			.getLogger(AddTagIDReaderHandler.class);
+	/**
+	 * Open the Readers view.
+	 */
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+
+		// Get the active window
+
+		IWorkbenchWindow window = HandlerUtil
+				.getActiveWorkbenchWindowChecked(event);
+		if (window == null)
+			return null;
+
+		// Get the active page
+
+		IWorkbenchPage page = window.getActivePage();
+		if (page == null)
+			return null;
+
+		// Start the addEventReader Wizard
+		try {
+			ReaderViewPart viewer = (ReaderViewPart) page.showView(ReaderViewPart.ID);
+			AddEventReaderWizard addWizard = new AddEventReaderWizard(viewer.getModel());
+			addWizard.init(window.getWorkbench());
+			WizardDialog dialog = new WizardDialog(window.getShell(),addWizard);
+			dialog.open();
+		} catch (PartInitException e) {
+			logger.error("Failed to open the Readers view", e);
+		}
+		
+		return null;
+	}
+
+}
