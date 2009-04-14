9087a4df3030ba22a58a732ef785ae889db28a76
jconlon <jconlon@0cc8e0f8-e01b-0410-8bcf-b9e3f263e42e>
2009-04-14 22:29:09 +0000

    CLOSED - # 369: Create a Fair Validation Constraint for Registered Animals http://localhost:8000/tracker/ticket/369

---
+++ tracker/trunk/com.verticon.tracker.fair.validation/src/com/verticon/tracker/fair/validation/constraints/AllAnimalsRegisteredConstraint.java
@@ -0,0 +1,65 @@
+package com.verticon.tracker.fair.validation.constraints;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.validation.AbstractModelConstraint;
+import org.eclipse.emf.validation.EMFEventType;
+import org.eclipse.emf.validation.IValidationContext;
+
+import com.verticon.tracker.Animal;
+import com.verticon.tracker.fair.Exhibit;
+import com.verticon.tracker.fair.Fair;
+
+public class AllAnimalsRegisteredConstraint extends AbstractModelConstraint {
+
+	
+	@Override
+	public IStatus validate(IValidationContext ctx) {
+		EObject eObj = ctx.getTarget();
+		EMFEventType eType = ctx.getEventType();
+		// In the case of batch mode.
+		if (eType == EMFEventType.NULL) {
+			if (eObj instanceof Animal) {
+				Animal animal = (Animal) eObj;
+				if(animalNeedsToBeRegistered(animal)){
+					
+					return ctx.createFailureStatus(new Object[] { animal });
+				}
+			}
+		} 
+		
+		return ctx.createSuccessStatus();
+
+	}
+
+	private boolean animalNeedsToBeRegistered(Animal animal){
+		boolean animalNeedsToBeRegistered = false;
+		if(animal.eResource()!=null && animal.eResource().getResourceSet()!=null){
+			ResourceSet rs = animal.eResource().getResourceSet();
+			Fair fair = getFair(rs);
+			if(fair != null){
+				animalNeedsToBeRegistered = true;
+				for (Exhibit exhibit : fair.exhibits()) {
+					if(animal.equals(exhibit.getAnimal())){
+						animalNeedsToBeRegistered = false;
+						break;
+					}
+				}
+			}
+		}
+		return animalNeedsToBeRegistered;
+	}
+	
+
+	 private Fair getFair(ResourceSet resourceSet) {
+		Resource modelResource = resourceSet
+		.getResources().get(0);
+		EObject eObject = modelResource.getContents().get(0);
+		if(eObject instanceof Fair){
+			return (Fair)eObject;
+		}
+		return null;
+	}
+}
