dd4df7597fdafaf3f81d2607a9b021fac6e5ea66
John Conlon <jconlon@verticon.com>
2009-04-20 18:54:09 +0000

    CLOSED - # 366: Popup Commands should be restricted to appropriate Editors http://localhost:8000/tracker/ticket/366
    Add a PropertyTester limit visibility and Refactored methods in Editors for returning Premises and Fair.

--- tracker/trunk/com.verticon.tracker.editor/src/com/verticon/tracker/editor/presentation/IPremisesProvider.java
+++ tracker/trunk/com.verticon.tracker.editor/src/com/verticon/tracker/editor/presentation/IPremisesProvider.java
@@ -16,6 +16,10 @@ public interface IPremisesProvider extends IEditingDomainProvider{
 	 */
 	public abstract void setSelectionToViewer(Collection<?> collection);
 	
+	/**
+	 * 
+	 * @return premises or null
+	 */
 	public Premises getPremises();
 
 }
