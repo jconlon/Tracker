b5eccdb1aa49f9e1d1f71988c2b4818265eb9dcc
John Conlon <jconlon@verticon.com>
2009-04-20 23:57:28 +0000

    CLOSED - # 366: Popup Commands should be restricted to appropriate Editors http://localhost:8000/tracker/ticket/366 ASSIGNED - # 365: Cleanup Exhibit Registration Wizard http://localhost:8000/tracker/ticket/365

---
+++ tracker/trunk/com.verticon.tracker.fair.editor/src/com/verticon/tracker/fair/editor/propertyTester/PersonRegisteredTester.java
@@ -0,0 +1,44 @@
+package com.verticon.tracker.fair.editor.propertyTester;
+
+import java.util.Collection;
+
+import org.eclipse.core.expressions.PropertyTester;
+
+import com.verticon.tracker.fair.Exhibit;
+import com.verticon.tracker.fair.Fair;
+import com.verticon.tracker.fair.Person;
+
+public class PersonRegisteredTester extends PropertyTester {
+	
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean test(Object receiver, String property, Object[] args,
+			Object expectedValue) {
+		
+		
+		if(receiver instanceof Collection){
+			Collection<Person> people = (Collection<Person>)receiver;
+			for (Person person : people) {//Everyperson must be registered 
+				if(!isPersonRegistered( person)){
+					return false;
+				}
+			}
+			
+			
+			return true;
+		}
+		
+		return false;
+	}
+
+	boolean isPersonRegistered(Person person){
+		Fair fair = (Fair)person.eContainer();
+		for (Exhibit exhibit : fair.exhibits()) {
+			if(exhibit.getExhibitor().equals(person)){
+				return true;
+			}
+		}
+		return false;
+	}
+}
