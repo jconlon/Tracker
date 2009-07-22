edae5aac68f5a6f2e953d1de9a5c590866c5f646
John Conlon <jconlon@verticon.com>
2009-07-22 22:28:14 +0000

    NEW - # 404: Add image for Dairy Bovine https://verticon-trac.sourcerepo.com/verticon_Tracker/ticket/404
    Adding this image required a regeneration of the Tracker Model.  This is the first regeneration since updating to Galileo and the latest EMF, therefore there are some subtle changes to many of the generated classes.

--- tracker/trunk/com.verticon.tracker.edit/src/com/verticon/tracker/edit/provider/TrackerItemProviderAdapterFactory.java
+++ tracker/trunk/com.verticon.tracker.edit/src/com/verticon/tracker/edit/provider/TrackerItemProviderAdapterFactory.java
@@ -1007,7 +1007,7 @@ public class TrackerItemProviderAdapterFactory extends TrackerAdapterFactory imp
 	public Object adapt(Object object, Object type) {
 		if (isFactoryForType(type)) {
 			Object adapter = super.adapt(object, type);
-			if (!(type instanceof Class) || (((Class<?>)type).isInstance(adapter))) {
+			if (!(type instanceof Class<?>) || (((Class<?>)type).isInstance(adapter))) {
 				return adapter;
 			}
 		}
