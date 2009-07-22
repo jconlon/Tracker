0bddd07f3d71545426aa289240058255a77db252
jconlon <jconlon@0cc8e0f8-e01b-0410-8bcf-b9e3f263e42e>
2009-07-22 19:22:08 +0000

    CLOSED - # 363: Wizard for updating event attributes needs a progress monitor https://verticon-trac.sourcerepo.com/verticon_Tracker/ticket/363

--- tracker/trunk/com.verticon.tracker.editor/src/com/verticon/tracker/editor/presentation/ModifyEventsWizard.java
+++ tracker/trunk/com.verticon.tracker.editor/src/com/verticon/tracker/editor/presentation/ModifyEventsWizard.java
@@ -3,9 +3,11 @@
  */
 package com.verticon.tracker.editor.presentation;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Collection;
 import java.util.Map;
 
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.emf.common.command.Command;
 import org.eclipse.emf.common.command.CompoundCommand;
 import org.eclipse.emf.ecore.EAttribute;
@@ -13,6 +15,7 @@ import org.eclipse.emf.edit.command.SetCommand;
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.IWorkbenchWindow;
@@ -22,7 +25,6 @@ import com.verticon.tracker.editor.util.ActionUtils;
 
 /**
  * Wizard for updating one or more events.
- * 
  * Pages:
  * 1. Select the attribute types to update
  * 2. Set the values of the attributes to update
@@ -50,7 +52,8 @@ public class ModifyEventsWizard extends Wizard {
 			wizardSettings = 
 				trackerSettings.addNewSection(MODIFY_EVENTS_WIZARD);
 		}
-		setDialogSettings(wizardSettings);		
+		setDialogSettings(wizardSettings);
+		
 	}
 
 	public void init(IWorkbenchWindow workbenchWindow, EditingDomain editingDomain,
@@ -58,6 +61,11 @@ public class ModifyEventsWizard extends Wizard {
 		this.workbenchWindow=workbenchWindow;
 		eventsToModify = ActionUtils.getSelectedEvents(selection);
 		this.editingDomain=editingDomain;
+	}
+	
+	@Override
+	public boolean needsProgressMonitor() {
+		return true;
 	}
 
 	@Override
@@ -71,19 +79,40 @@ public class ModifyEventsWizard extends Wizard {
 
 	@Override
 	public boolean performFinish() {
-		Map<EAttribute, Object> changesToMake = selectModifyEventsValuesPage.getChangesToMakeOnSelectedEvents();
+		final Map<EAttribute, Object> changesToMake = 
+			selectModifyEventsValuesPage.getChangesToMakeOnSelectedEvents();
 		String attWording = changesToMake.size()==1?" attribute ": " attributes ";
-		String eventWording = eventsToModify.size()==1?" event.": " events.";
+		final String eventWording = eventsToModify.size()==1?" event.": " events.";
 		try {
-			editingDomain.getCommandStack().execute(createCommand(changesToMake));
-			
-			
-			
-		} catch (RuntimeException e) {
+			getContainer().run(false, false, new IRunnableWithProgress(){
+
+				@Override
+				public void run(final IProgressMonitor monitor)
+						throws InvocationTargetException, InterruptedException {
+					 try {
+						monitor.beginTask(
+								"Modifying "+eventsToModify.size()+' '+eventWording+"..",
+								eventsToModify.size());
+						
+						for (Event event : eventsToModify) {
+							CompoundCommand command = createCommand(changesToMake, event);	
+							editingDomain.getCommandStack().execute(
+									command);
+							monitor.worked(1);	
+						}
+						
+					} finally {
+						monitor.done();
+					}
+				}
+				
+			});
+		} catch (Exception e) {
 			MessageDialog.openError(workbenchWindow.getShell(), 
 					"Modified Events Failed", "Failed to modify events because: " + e);
 			return false;
 		}
+
 		MessageDialog.openInformation(workbenchWindow.getShell(), 
 				"Modified Events", "Modified "+changesToMake.size()+
 				attWording+
@@ -92,21 +121,21 @@ public class ModifyEventsWizard extends Wizard {
 		return true;
 	}
 	
-	private Command createCommand(Map<EAttribute, Object> changesToMake){
+
+	private CompoundCommand createCommand(Map<EAttribute, Object> changesToMake,
+			 Event event) {
 		CompoundCommand compoundCommand = new CompoundCommand();
-		Command command = null;
-		for (Event event : eventsToModify) {
-			for(Map.Entry<EAttribute, Object> entry :changesToMake.entrySet()){
-				command = SetCommand.create(
-						editingDomain, //Domain
-						 event, //Owner
-						 entry.getKey(), //Feature
-						 entry.getValue()//value
-				);
-				if(command !=null){
-					compoundCommand.append(command);
-				}
-			}		
+		Command command;
+		for(Map.Entry<EAttribute, Object> entry :changesToMake.entrySet()){
+			command = SetCommand.create(
+					editingDomain, //Domain
+					 event, //Owner
+					 entry.getKey(), //Feature
+					 entry.getValue()//value
+			);
+			if(command !=null){
+				compoundCommand.append(command);
+			}
 		}
 		return compoundCommand;
 	}
@@ -118,4 +147,5 @@ public class ModifyEventsWizard extends Wizard {
 		return selectEventAttributesWizardPage.getSelectedAttributes();
 	} 
 
+	
 }
