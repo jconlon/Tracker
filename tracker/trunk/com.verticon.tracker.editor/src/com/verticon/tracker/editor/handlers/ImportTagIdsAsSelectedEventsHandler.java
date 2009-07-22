178daf631be1a4f29353fb43aec71fb5c8683669
John Conlon <jconlon@verticon.com>
2009-07-22 02:17:27 +0000

    CLOSED - # 403: Exception being thrown in Modify Event Attributes Wizard https://verticon-trac.sourcerepo.com/verticon_Tracker/ticket/403

--- tracker/trunk/com.verticon.tracker.editor/src/com/verticon/tracker/editor/handlers/ImportTagIdsAsSelectedEventsHandler.java
+++ tracker/trunk/com.verticon.tracker.editor/src/com/verticon/tracker/editor/handlers/ImportTagIdsAsSelectedEventsHandler.java
@@ -6,11 +6,9 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.ISources;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 import com.verticon.tracker.editor.presentation.AddTagIdsAnimalAndEventWizard;
-import com.verticon.tracker.editor.presentation.IPremisesProvider;
 
 /**
  * Handler associated with a selection on a Capture *.tags file.
@@ -32,33 +30,6 @@ import com.verticon.tracker.editor.presentation.IPremisesProvider;
  * 
  */
 public class ImportTagIdsAsSelectedEventsHandler extends AbstractHandler{
-
-	/**
-	 *  workaround for  https://bugs.eclipse.org/bugs/show_bug.cgi?id=201743 
-	 *  Scheduled for fix in 3.5
-	 */
-	private boolean isEnabled;
-	
-	/**
-	 *  workaround for  https://bugs.eclipse.org/bugs/show_bug.cgi?id=201743 
-	 *  Scheduled for fix in 3.5
-	 */
-	@Override
-	public boolean isEnabled() {
-		return isEnabled;
-	}
-
-	/**
-	 *  workaround for  https://bugs.eclipse.org/bugs/show_bug.cgi?id=201743 
-	 *  Scheduled for fix in 3.5
-	 */
-	@Override
-	public void setEnabled(Object evaluationContext) {
-		IEditorPart editorPart = (IEditorPart) HandlerUtil.getVariable(evaluationContext, ISources.ACTIVE_EDITOR_NAME);
-		isEnabled = 
-				editorPart.getAdapter(IPremisesProvider.class) != null;
-		super.setEnabled(evaluationContext);
-	}
 
 	/*
 	 * (non-Javadoc)
