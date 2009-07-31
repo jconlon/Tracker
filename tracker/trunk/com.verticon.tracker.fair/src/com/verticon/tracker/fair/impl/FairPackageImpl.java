f124cca244452e63456028eb8ec0857b92e65fa6
jconlon <jconlon@0cc8e0f8-e01b-0410-8bcf-b9e3f263e42e>
2009-07-31 20:40:17 +0000

    NEW - # 420: Remove unused code and clean up warnings https://verticon-trac.sourcerepo.com/verticon_Tracker/ticket/420

--- tracker/trunk/com.verticon.tracker.fair/src/com/verticon/tracker/fair/impl/FairPackageImpl.java
+++ tracker/trunk/com.verticon.tracker.fair/src/com/verticon/tracker/fair/impl/FairPackageImpl.java
@@ -151,20 +151,10 @@ public class FairPackageImpl extends EPackageImpl implements FairPackage {
 	private static boolean isInited = false;
 
 	/**
-	 * Creates, registers, and initializes the <b>Package</b> for this
-	 * model, and for any others upon which it depends.  Simple
-	 * dependencies are satisfied by calling this method on all
-	 * dependent packages before doing anything else.  This method drives
-	 * initialization for interdependent packages directly, in parallel
-	 * with this package, itself.
-	 * <p>Of this package and its interdependencies, all packages which
-	 * have not yet been registered by their URI values are first created
-	 * and registered.  The packages are then initialized in two steps:
-	 * meta-model objects for all of the packages are created before any
-	 * are initialized, since one package's meta-model objects may refer to
-	 * those of another.
-	 * <p>Invocation of this method will not affect any packages that have
-	 * already been initialized.
+	 * Creates, registers, and initializes the <b>Package</b> for this model, and for any others upon which it depends.
+	 * 
+	 * <p>This method is used to initialize {@link FairPackage#eINSTANCE} when that field is accessed.
+	 * Clients should not invoke it directly. Instead, they should simply access that field to obtain the package.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
 	 * @see #eNS_URI
@@ -176,7 +166,7 @@ public class FairPackageImpl extends EPackageImpl implements FairPackage {
 		if (isInited) return (FairPackage)EPackage.Registry.INSTANCE.getEPackage(FairPackage.eNS_URI);
 
 		// Obtain or create and register package
-		FairPackageImpl theFairPackage = (FairPackageImpl)(EPackage.Registry.INSTANCE.getEPackage(eNS_URI) instanceof FairPackageImpl ? EPackage.Registry.INSTANCE.getEPackage(eNS_URI) : new FairPackageImpl());
+		FairPackageImpl theFairPackage = (FairPackageImpl)(EPackage.Registry.INSTANCE.get(eNS_URI) instanceof FairPackageImpl ? EPackage.Registry.INSTANCE.get(eNS_URI) : new FairPackageImpl());
 
 		isInited = true;
 
@@ -192,6 +182,9 @@ public class FairPackageImpl extends EPackageImpl implements FairPackage {
 		// Mark meta-data to indicate it can't be changed
 		theFairPackage.freeze();
 
+  
+		// Update the registry and return the package
+		EPackage.Registry.INSTANCE.put(FairPackage.eNS_URI, theFairPackage);
 		return theFairPackage;
 	}
 
