bdc8315d345aa7764452d4d0c56e4ea455fe0310
John Conlon <jconlon@verticon.com>
2009-08-13 01:42:13 +0000

    ASSIGNED - # 420: Remove unused code and clean up warnings https://verticon-trac.sourcerepo.com/verticon_Tracker/ticket/420

--- tracker/trunk/com.verticon.tracker.ocl.query.viewer.tests/src/com/verticon/tracker/ocl/query/viewer/Constants.java
+++ tracker/trunk/com.verticon.tracker.ocl.query.viewer.tests/src/com/verticon/tracker/ocl/query/viewer/Constants.java
@@ -8,13 +8,15 @@ import org.slf4j.MarkerFactory;
  * @author jconlon
  *
  */
-public class Constants {
+@SuppressWarnings("ucd")
+class Constants {
 
 	private static final String ID = "com.verticon.tracker.ocl.query.viewer";
 	/**
 	 * slf4j Marker to keep track of bundle
 	 */
-	public static final Marker bundleMarker = createBundleMarker();
+	@SuppressWarnings("ucd")
+	protected static final Marker bundleMarker = createBundleMarker();
 	private static final Marker createBundleMarker() {
 		Marker bundleMarker = MarkerFactory.getMarker(ID);
 		bundleMarker.add(MarkerFactory.getMarker("IS_TEST_TRACKER"));
