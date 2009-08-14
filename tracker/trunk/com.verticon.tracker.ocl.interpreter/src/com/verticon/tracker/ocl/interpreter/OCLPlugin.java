41cd4e72e3ae9718c6fb00c9d04596ba084208e7
John Conlon <jconlon@verticon.com>
2009-08-14 18:44:07 +0000

    ASSIGNED - # 420: Remove unused code and clean up warnings https://verticon-trac.sourcerepo.com/verticon_Tracker/ticket/420

--- tracker/trunk/com.verticon.tracker.ocl.interpreter/src/com/verticon/tracker/ocl/interpreter/OCLPlugin.java
+++ tracker/trunk/com.verticon.tracker.ocl.interpreter/src/com/verticon/tracker/ocl/interpreter/OCLPlugin.java
@@ -23,6 +23,7 @@ import org.osgi.framework.BundleContext;
 /**
  * The main plugin class to be used in the desktop.
  */
+@SuppressWarnings("ucd")
 public class OCLPlugin extends AbstractUIPlugin {
 
 	
