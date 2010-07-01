9e9fae0785a1341d477643f2db4f5b4a5292eb14
jconlon <jconlon@0cc8e0f8-e01b-0410-8bcf-b9e3f263e42e>
2010-07-01 16:24:43 +0000

    ASSIGNED - # 300: Fix copyright and license notices. https://verticon-trac.sourcerepo.com/verticon_Tracker/ticket/300

--- tracker/trunk/com.verticon.tracker.ocl.query/src/com/verticon/tracker/ocl/query/actions/AbstractQueryDelegate.java
+++ tracker/trunk/com.verticon.tracker.ocl.query/src/com/verticon/tracker/ocl/query/actions/AbstractQueryDelegate.java
@@ -1,19 +1,13 @@
-/**
- * <copyright>
- *
- * Copyright (c) 2005 IBM Corporation and others.
- * All rights reserved.   This program and the accompanying materials
+/*******************************************************************************
+ * Copyright (c) 2010 Verticon, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *   IBM - Initial API and implementation
- *
- * </copyright>
- *
- * $Id$
- */
+ *    Verticon, Inc. - initial API and implementation
+ *******************************************************************************/
 
 package com.verticon.tracker.ocl.query.actions;
 
--- tracker/trunk/com.verticon.tracker.ocl.query/src/com/verticon/tracker/ocl/query/wizards/IOCLQueryWizardPage.java
+++ tracker/trunk/com.verticon.tracker.ocl.query/src/com/verticon/tracker/ocl/query/wizards/IOCLQueryWizardPage.java
@@ -1,19 +1,13 @@
-/**
- * <copyright>
- *
- * Copyright (c) 2005 IBM Corporation and others.
- * All rights reserved.   This program and the accompanying materials
+/*******************************************************************************
+ * Copyright (c) 2010 Verticon, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *   IBM - Initial API and implementation
- *
- * </copyright>
- *
- * $Id: IOCLQueryWizardPage.java,v 1.1 2006/02/13 16:12:04 cdamus Exp $
- */
+ *    Verticon, Inc. - initial API and implementation
+ *******************************************************************************/
 
 package com.verticon.tracker.ocl.query.wizards;
 
