3dfa7cda5699809a69ca5bfe00ca2911dbdf8dea
jconlon <jconlon@0cc8e0f8-e01b-0410-8bcf-b9e3f263e42e>
2010-07-02 19:21:08 +0000

    CLOSED - # 300: Fix copyright and license notices. https://verticon-trac.sourcerepo.com/verticon_Tracker/ticket/300 CLOSED - # 580: Update to Eclipse Helios dependencies https://verticon-trac.sourcerepo.com/verticon_Tracker/ticket/580

--- tracker/trunk/com.verticon.tracker.fair.poi/src/com/verticon/tracker/fair/poi/FairPoiPlugin.java
+++ tracker/trunk/com.verticon.tracker.fair.poi/src/com/verticon/tracker/fair/poi/FairPoiPlugin.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Verticon, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Verticon, Inc. - initial API and implementation
+ *******************************************************************************/
 /**
  * 
  */
@@ -20,7 +30,6 @@ public class FairPoiPlugin extends AbstractUIPlugin {
 	 * slf4j Logger
 	 */
 	private final Logger logger = LoggerFactory.getLogger(FairPoiPlugin.class);
-
 	private static final String ID = "com.verticon.tracker.fair.poi";
 
 	@Override
