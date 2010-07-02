f49b8aa751ef18d22cd27a67bc7752ed954b6587
John Conlon <jconlon@verticon.com>
2010-07-02 19:23:39 +0000

    CLOSED - # 300: Fix copyright and license notices. https://verticon-trac.sourcerepo.com/verticon_Tracker/ticket/300 CLOSED - # 580: Update to Eclipse Helios dependencies https://verticon-trac.sourcerepo.com/verticon_Tracker/ticket/580

--- tracker/trunk/com.verticon.tracker.fair/src/com/verticon/tracker/fair/impl/PersonImpl.java
+++ tracker/trunk/com.verticon.tracker.fair/src/com/verticon/tracker/fair/impl/PersonImpl.java
@@ -1,8 +1,13 @@
-/**
- * Copyright 2007 Verticon, Inc. All Rights Reserved.
+/*******************************************************************************
+ * Copyright (c) 2010 Verticon, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * $Id$
- */
+ * Contributors:
+ *    Verticon, Inc. - initial API and implementation
+ *******************************************************************************/
 package com.verticon.tracker.fair.impl;
 
 import org.eclipse.emf.common.notify.Notification;
