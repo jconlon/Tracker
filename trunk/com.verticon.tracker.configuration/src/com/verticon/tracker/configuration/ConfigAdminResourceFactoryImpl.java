bfadb34fbdc40361c5b846d004e6b80bfcbbda4b
John Conlon <jconlon@verticon.com>
2010-07-23 19:31:06 +0000

    ACCEPTED - # 578: Create feature for generic iRouter components. https://verticon-trac.sourcerepo.com/verticon_Tracker/ticket/578 CLOSED - # 551: Add support for configuring iRouter components https://verticon-trac.sourcerepo.com/verticon_Tracker/ticket/551

---
+++ trunk/com.verticon.tracker.configuration/src/com/verticon/tracker/configuration/ConfigAdminResourceFactoryImpl.java
@@ -0,0 +1,32 @@
+package com.verticon.tracker.configuration;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.emf.ecore.xmi.impl.XMLResourceImpl;
+
+import com.verticon.tracker.configuration.internal.ConfigAdminResouceImpl;
+
+public class ConfigAdminResourceFactoryImpl implements Resource.Factory {
+
+	public ConfigAdminResourceFactoryImpl() {
+		super();
+	}
+
+	@Override
+	public Resource createResource(URI uri) {
+		XMLResourceImpl result = new ConfigAdminResouceImpl(uri);
+		
+		result.getDefaultSaveOptions().put(XMLResource.OPTION_EXTENDED_META_DATA, Boolean.TRUE);
+		result.getDefaultLoadOptions().put(XMLResource.OPTION_EXTENDED_META_DATA, Boolean.TRUE);
+
+		result.getDefaultSaveOptions().put(XMLResource.OPTION_SCHEMA_LOCATION, Boolean.TRUE);
+
+		result.getDefaultLoadOptions().put(XMLResource.OPTION_USE_ENCODED_ATTRIBUTE_STYLE, Boolean.TRUE);
+		result.getDefaultSaveOptions().put(XMLResource.OPTION_USE_ENCODED_ATTRIBUTE_STYLE, Boolean.TRUE);
+
+		result.getDefaultLoadOptions().put(XMLResource.OPTION_USE_LEXICAL_HANDLER, Boolean.TRUE);
+		return result;
+	}
+
+}
