b96740f2065bac3e8dc5465a7d3bb678cdfbff2f
John Conlon <jconlon@verticon.com>
2010-08-30 21:47:44 +0000

    CLOSED - # 591: Create device simulator for Mettler Balance https://verticon-trac.sourcerepo.com/verticon_Tracker/ticket/591
    This adds an infrastructure for creating devices...

---
+++ trunk/com.verticon.tracker.irouter.dev/src/com/verticon/tracker/irouter/dev/Constants.java
@@ -0,0 +1,6 @@
+package com.verticon.tracker.irouter.dev;
+
+public class Constants {
+	public enum DeviceType{SERVER_SOCKET, SERVER_COMM_PORT}
+	public enum Property{PREFIX, TYPE, PORT, SOCKET};
+}
