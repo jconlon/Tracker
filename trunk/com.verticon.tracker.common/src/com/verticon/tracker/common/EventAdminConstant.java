064c41f33bacffd7eda0bed04aee7b84132b22e9
jconlon <jconlon@0cc8e0f8-e01b-0410-8bcf-b9e3f263e42e>
2010-09-04 21:57:54 +0000

    REOPENED - # 578: Create feature for generic iRouter components. https://verticon-trac.sourcerepo.com/verticon_Tracker/ticket/578

---
+++ trunk/com.verticon.tracker.common/src/com/verticon/tracker/common/EventAdminConstant.java
@@ -0,0 +1,28 @@
+package com.verticon.tracker.common;
+
+public enum EventAdminConstant {
+	/**
+	 * EventAdmin topic jointly used by iRouter and Tracker
+	 */
+	EVENT_ADMIN_TOPIC("com/verticon/tracker/event"), 
+	
+	/**
+	 * Measurement transactions can be sent to the eventadmin as payloads
+	 */
+	IROUTER_PAYLOAD("com.verticon.tracker.irouter.payload");
+	
+	private final String propertyValue;
+	
+	private EventAdminConstant(String value){
+		this.propertyValue=value;
+	}
+	
+	/**
+	 * 
+	 * @return property propertyValue
+	 */
+	public String toProp() {
+		return propertyValue;
+	}
+
+}
