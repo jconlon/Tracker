66270c8189fca8559027de53b1ef288f44082d3f
jconlon <jconlon@0cc8e0f8-e01b-0410-8bcf-b9e3f263e42e>
2010-10-19 18:34:23 +0000

    CLOSED - # 618: Add option to hide edge labels in Monitor viewer https://verticon-trac.sourcerepo.com/verticon_Tracker/ticket/618

--- tracker/trunk/com.verticon.tracker.irouter.monitor.view/src/com/verticon/tracker/irouter/monitor/view/internal/MonitorView.java
+++ tracker/trunk/com.verticon.tracker.irouter.monitor.view/src/com/verticon/tracker/irouter/monitor/view/internal/MonitorView.java
@@ -11,14 +11,23 @@
 package com.verticon.tracker.irouter.monitor.view.internal;
 
 import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.ControlContribution;
 import org.eclipse.jface.action.IMenuListener;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.action.Separator;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.ui.IActionBars;
+import org.eclipse.ui.ISharedImages;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.forms.ManagedForm;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.ui.forms.widgets.ScrolledForm;
@@ -30,18 +39,19 @@ import org.eclipse.zest.core.viewers.ZoomContributionViewItem;
 
 public class MonitorView extends ViewPart implements IZoomableWorkbenchPart {
 
+	enum Layout {
+		HORIZONTAL, SPRING, GRID, RADIAL, TREE
+	};
+
 	private FormToolkit toolKit = null;
 	private ScrolledForm form = null;
 	private ManagedForm managedForm = null;
 	private final MonitorMasterDetailsBlock block;
 	private ZoomContributionViewItem contextZoomContributionViewItem;
 	private ZoomContributionViewItem toolbarZoomContributionViewItem;
-	private Action setTreeLayoutAction;
-	private Action setRadialLayoutAction;
-	private Action setGridLayoutAction;
-	private Action setSpringLayoutAction;
-	private Action setHorizontalLayoutAction;
 	private Action refresh;
+	private Action showConnectionLabels;
+	private LayoutCombo myCombo;
 
 	public MonitorView() {
 		super();
@@ -55,7 +65,7 @@ public class MonitorView extends ViewPart implements IZoomableWorkbenchPart {
 		managedForm = new ManagedForm(this.toolKit, this.form);
 		form.setText("iRouter Monitor");
 		block.createContent(managedForm);
-
+		myCombo = new LayoutCombo("Layout");
 		toolbarZoomContributionViewItem = new ZoomContributionViewItem(this);
 		contextZoomContributionViewItem = new ZoomContributionViewItem(this);
 		makeActions();
@@ -65,7 +75,7 @@ public class MonitorView extends ViewPart implements IZoomableWorkbenchPart {
 
 	@Override
 	public void setFocus() {
-		
+
 	}
 
 	/**
@@ -88,16 +98,19 @@ public class MonitorView extends ViewPart implements IZoomableWorkbenchPart {
 		getSite().registerContextMenu(menuMgr, viewer);
 
 	}
+
 	@Override
 	public AbstractZoomableViewer getZoomableViewer() {
 		return block.getViewer();
 	}
-	
+
 	/**
 	 * Set the toolbar
 	 */
 	private void fillToolBar() {
 		IActionBars bars = getViewSite().getActionBars();
+		bars.getMenuManager().add(showConnectionLabels);
+		bars.getMenuManager().add(new Separator());
 		bars.getMenuManager().add(toolbarZoomContributionViewItem);
 
 		fillLocalToolBar(bars.getToolBarManager());
@@ -111,15 +124,13 @@ public class MonitorView extends ViewPart implements IZoomableWorkbenchPart {
 	 */
 	private void fillLocalToolBar(IToolBarManager toolBarManager) {
 		toolBarManager.add(new Separator());
-		toolBarManager.add(setTreeLayoutAction);
-		toolBarManager.add(setRadialLayoutAction);
-		toolBarManager.add(setGridLayoutAction);
-		toolBarManager.add(setSpringLayoutAction);
-		toolBarManager.add(setHorizontalLayoutAction);
+
+		toolBarManager.add(myCombo);
+
 		toolBarManager.add(new Separator());
 		toolBarManager.add(refresh);
 	}
-	
+
 	/**
 	 * Add the items to the context menu
 	 * 
@@ -127,13 +138,7 @@ public class MonitorView extends ViewPart implements IZoomableWorkbenchPart {
 	 */
 	private void fillContextMenu(IMenuManager manager) {
 		manager.add(new Separator());
-
-		manager.add(new Separator());
-		manager.add(setGridLayoutAction);
-		manager.add(setSpringLayoutAction);
-		manager.add(setHorizontalLayoutAction);
-		manager.add(setTreeLayoutAction);
-		manager.add(setRadialLayoutAction);
+		manager.add(myCombo);
 		manager.add(new Separator());
 		manager.add(refresh);
 		manager.add(new Separator());
@@ -145,46 +150,6 @@ public class MonitorView extends ViewPart implements IZoomableWorkbenchPart {
 	 * includes: - Change Layout...
 	 */
 	private void makeActions() {
-		setTreeLayoutAction = new Action() {
-			public void run() {
-				MonitorMasterDetailsBlock.setTreeLayout(block.getViewer());
-			}
-		};
-		setTreeLayoutAction.setText("Tree");
-		setTreeLayoutAction.setToolTipText("Set the layout of the graph to a tree like view");
-
-		setRadialLayoutAction = new Action() {
-			public void run() {
-				MonitorMasterDetailsBlock.setRadialLayout(block.getViewer());
-			}
-		};
-		setRadialLayoutAction.setText("Radial");
-		setRadialLayoutAction.setToolTipText("Set the layout of the graph to a circular like view");
-
-		setGridLayoutAction = new Action() {
-			public void run() {
-				MonitorMasterDetailsBlock.setGridLayout(block.getViewer());
-			}
-		};
-		setGridLayoutAction.setText("Grid");
-		setGridLayoutAction.setToolTipText("Set the layout of the graph to a grid like view");
-
-		setSpringLayoutAction = new Action() {
-			public void run() {
-				MonitorMasterDetailsBlock.setSpringLayout(block.getViewer());
-			}
-		};
-		setSpringLayoutAction.setText("Spring");
-		setSpringLayoutAction.setToolTipText("Set the layout of the graph to a spring like view");
-
-		setHorizontalLayoutAction = new Action() {
-			public void run() {
-				MonitorMasterDetailsBlock.setHorizontalLayout(block.getViewer());
-			}
-		};
-		setHorizontalLayoutAction.setText("Horizontal");
-		setHorizontalLayoutAction.setToolTipText("Set the layout of the graph to a horizontal like view");
-
 		refresh = new Action() {
 			public void run() {
 				block.getViewer().refresh(true);
@@ -192,6 +157,80 @@ public class MonitorView extends ViewPart implements IZoomableWorkbenchPart {
 		};
 		refresh.setText("Refresh");
 		refresh.setToolTipText("Refresh the graph.");
+		refresh.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages()
+				.getImageDescriptor(ISharedImages.IMG_ELCL_SYNCED));
+
+		showConnectionLabels = new Action("Hide Wire Names",
+				Action.AS_CHECK_BOX) {
+			public void run() {
+				block.setShowWireLabels(!this.isChecked());
+				block.getViewer().refresh(true);
+			}
+		};
+
+		showConnectionLabels.setToolTipText("Shows the connection labels");
 
 	}
+
+	class LayoutCombo extends ControlContribution {
+
+		protected LayoutCombo(String id) {
+			super(id);
+		}
+
+		@Override
+		protected Control createControl(Composite parent) {
+			final Combo combo = new Combo(parent, SWT.NULL);
+			combo.setToolTipText("Sets the type of layout to use for drawing the graph.");
+
+			for (Layout layout : Layout.values()) {
+				combo.add(layout.name());
+			}
+
+			combo.addSelectionListener(new SelectionListener() {
+				public void widgetSelected(SelectionEvent e) {
+
+					switch (Layout.valueOf(combo.getText())) {
+					case HORIZONTAL:
+						MonitorMasterDetailsBlock.setHorizontalLayout(block
+								.getViewer());
+						break;
+
+					case SPRING:
+						MonitorMasterDetailsBlock.setSpringLayout(block
+								.getViewer());
+						break;
+
+					case GRID:
+						MonitorMasterDetailsBlock.setGridLayout(block
+								.getViewer());
+						break;
+
+					case RADIAL:
+						MonitorMasterDetailsBlock.setRadialLayout(block
+								.getViewer());
+						break;
+
+					case TREE:
+						MonitorMasterDetailsBlock.setTreeLayout(block
+								.getViewer());
+						break;
+					}
+				}
+
+				public void widgetDefaultSelected(SelectionEvent e) {
+					MonitorMasterDetailsBlock.setHorizontalLayout(block
+							.getViewer());
+				}
+			});
+			GridData gridData = new GridData(SWT.FILL, SWT.TOP, false, false);
+			gridData.widthHint = 20;
+			gridData.heightHint = SWT.DEFAULT;
+			combo.setLayoutData(gridData);
+			combo.pack();
+			return combo;
+		}
+
+	}
+
 }
