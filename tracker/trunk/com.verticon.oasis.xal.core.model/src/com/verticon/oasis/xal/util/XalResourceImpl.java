9c43b67e20f6807f13bd9ca48d4f5efe2b590dcf
John Conlon <jconlon@verticon.com>
2011-03-14 16:56:40 +0000

    CLOSED - # 704: Add kml model and document editor https://verticon-trac.sourcerepo.com/verticon_Tracker/ticket/704

---
+++ tracker/trunk/com.verticon.oasis.xal.core.model/src/com/verticon/oasis/xal/util/XalResourceImpl.java
@@ -0,0 +1,34 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package com.verticon.oasis.xal.util;
+
+import org.eclipse.emf.common.util.URI;
+
+import org.eclipse.emf.ecore.xmi.impl.XMLResourceImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Resource </b> associated with the package.
+ * <!-- end-user-doc -->
+ * @see com.verticon.oasis.xal.util.XalResourceFactoryImpl
+ * @generated
+ */
+public class XalResourceImpl extends XMLResourceImpl {
+	/**
+	 * Creates an instance of the resource.
+	 * <!-- begin-user-doc -->
+	 * Set encoding to UTF-8
+	 * <!-- end-user-doc -->
+	 * @param uri the URI of the new resource.
+	 * @generated NOT
+	 */
+	public XalResourceImpl(URI uri) {
+		super(uri);
+		setEncoding("UTF-8");
+	}
+
+} //XalResourceImpl
