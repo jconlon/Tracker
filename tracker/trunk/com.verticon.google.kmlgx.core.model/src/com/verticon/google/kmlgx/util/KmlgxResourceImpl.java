366ec12228e5689f202aea046c8d49c042fc86cb
jconlon <jconlon@0cc8e0f8-e01b-0410-8bcf-b9e3f263e42e>
2011-03-14 16:46:35 +0000

    CLOSED - # 704: Add kml model and document editor https://verticon-trac.sourcerepo.com/verticon_Tracker/ticket/704

---
+++ tracker/trunk/com.verticon.google.kmlgx.core.model/src/com/verticon/google/kmlgx/util/KmlgxResourceImpl.java
@@ -0,0 +1,32 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package com.verticon.google.kmlgx.util;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.xmi.impl.XMLResourceImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Resource </b> associated with the package.
+ * <!-- end-user-doc -->
+ * @see com.verticon.google.kmlgx.util.KmlgxResourceFactoryImpl
+ * @generated
+ */
+public class KmlgxResourceImpl extends XMLResourceImpl {
+	/**
+	 * Creates an instance of the resource.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param uri the URI of the new resource.
+	 * @generated NOT
+	 */
+	public KmlgxResourceImpl(URI uri) {
+		super(uri);
+		setEncoding("UTF-8");
+	}
+
+} //KmlgxResourceImpl
