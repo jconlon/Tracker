014cffd62b5e7e128df6f6adb4cd2a6429aff420
jconlon <jconlon@0cc8e0f8-e01b-0410-8bcf-b9e3f263e42e>
2011-04-11 20:23:37 +0000

    CLOSED - # 722: Adding a Reader does not always display a page for the template. https://verticon-trac.sourcerepo.com/verticon_Tracker/ticket/722

--- tracker/trunk/com.verticon.tracker.reader/src/com/verticon/tracker/reader/wizards/AddEventReaderWizard.java
+++ tracker/trunk/com.verticon.tracker.reader/src/com/verticon/tracker/reader/wizards/AddEventReaderWizard.java
@@ -14,15 +14,22 @@
 package com.verticon.tracker.reader.wizards;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceVisitor;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.jface.wizard.IWizardNode;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PartInitException;
 
 import com.verticon.tracker.editor.presentation.SelectAnimalDocumentWizardPage;
 import com.verticon.tracker.editor.util.AnimalTemplateBean;
+import com.verticon.tracker.editor.util.TrackerEditorUtils;
 import com.verticon.tracker.reader.IReader;
 import com.verticon.tracker.reader.IReaderWizard;
 import com.verticon.tracker.reader.ReaderPlugin;
@@ -42,6 +49,9 @@ import com.verticon.tracker.reader.views.ReaderViewModel;
 public class AddEventReaderWizard extends Wizard {
 
 	private static final String WIZARD_TITLE = "Add an Event Reader";
+	private static final String APPEND_INSTRUCTION = 
+		"\n\nThe Add Reader Wizard looks for animal template files inside the project associated with the active editor."+
+		" To add a Reader, make sure that the active editor has opened a file from a project that contains at least one animal template file.";
 	private static final String WIZARD_DIALOG_TAG = "AddReaderWizard";
 
 	private final ReaderViewModel readerViewModel;
@@ -50,6 +60,8 @@ public class AddEventReaderWizard extends Wizard {
 	private SelectAnimalDocumentWizardPage selectAnimalDocumentWizardPage;
 
 	private EventReaderWizardSelectionPage publisherWizardSelection;
+	
+	private IProject projectWithTemplate = null;
 
 	public AddEventReaderWizard(ReaderViewModel readerViewModel) {
 		super();
@@ -77,12 +89,46 @@ public class AddEventReaderWizard extends Wizard {
 	}
 
 	public void init(IWorkbench workbench) throws PartInitException {
-		
-		if(workbench.getActiveWorkbenchWindow()
-		.getActivePage().getActiveEditor() == null){
-		   throw new PartInitException("Can't find an Editor in the workbench. Please open a Premises document.");
+		projectWithTemplate = hasTemplates( workbench);
+		if(projectWithTemplate==null){
+			throw new PartInitException("Could not find any animal templates in the active editor's project."+APPEND_INSTRUCTION);
 		}
 		this.workbench = workbench;
+	}
+	
+	private IProject hasTemplates(IWorkbench iworkbench)throws PartInitException {
+		IProject result = null;
+		//Project
+		if (iworkbench != null){
+			IWorkbenchWindow iworkbenchwindow = iworkbench.getActiveWorkbenchWindow();
+				if (iworkbenchwindow != null){
+						IWorkbenchPage iworkbenchpage = iworkbenchwindow.getActivePage();
+							if (iworkbenchpage != null){
+								IEditorPart editor = iworkbenchpage.getActiveEditor();
+								if(editor ==null){
+									throw new PartInitException("Could not find an active editor."+APPEND_INSTRUCTION);
+								}else{
+									IResource resource = TrackerEditorUtils.extractResource(editor);
+									if(resource == null){
+										throw new PartInitException("Could not find any resources in the active editor."+APPEND_INSTRUCTION);
+									}else{
+										IProject project = resource.getProject();
+										TemplateVisitor visitor = new TemplateVisitor();
+										try {
+											project.accept(visitor);
+											result = visitor.foundTemplate?project:null;
+										} catch (CoreException e) {
+											throw new PartInitException(e.getLocalizedMessage());
+										}
+									}
+
+								}
+
+							}
+				}
+		}
+
+		return result;
 	}
 
 	/*
@@ -113,14 +159,23 @@ public class AddEventReaderWizard extends Wizard {
 		selectAnimalDocumentWizardPage = new SelectAnimalDocumentWizardPage();
 		selectAnimalDocumentWizardPage.setTitle("Select an Animal Document to use as a template for the Event Publisher");
 		addPage(selectAnimalDocumentWizardPage);
-
-		IEditorPart editor = workbench.getActiveWorkbenchWindow()
-				.getActivePage().getActiveEditor();
-		selectAnimalDocumentWizardPage.init(editor);
+		selectAnimalDocumentWizardPage.init(projectWithTemplate);
 
 		publisherWizardSelection = new EventReaderWizardSelectionPage(
 				"Select Event Reader", workbench);
 		addPage(publisherWizardSelection);
+	}
+	
+	private class TemplateVisitor implements IResourceVisitor{
+		boolean foundTemplate = false;
+		@Override
+		public boolean visit(IResource resource) throws CoreException {
+			if(resource.getFileExtension()!=null && resource.getFileExtension().equals("animal")){
+				foundTemplate = true;
+			}
+			return !foundTemplate;
+		}
+		
 	}
 
 }
