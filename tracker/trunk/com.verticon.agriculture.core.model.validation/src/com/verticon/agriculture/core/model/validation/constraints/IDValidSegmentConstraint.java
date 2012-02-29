ac8a4fbfa063571d1314166e8f21d222b7c23732
jconlon <jconlon@0cc8e0f8-e01b-0410-8bcf-b9e3f263e42e>
2012-02-29 00:05:45 +0000

    CLOSED - # 767: Constrain Location id attribute to be a valid URI segment https://verticon-trac.sourcerepo.com/verticon_Tracker/ticket/767

---
+++ tracker/trunk/com.verticon.agriculture.core.model.validation/src/com/verticon/agriculture/core/model/validation/constraints/IDValidSegmentConstraint.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Verticon, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Verticon, Inc. - initial API and implementation
+ *******************************************************************************/
+package com.verticon.agriculture.core.model.validation.constraints;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.validation.AbstractModelConstraint;
+import org.eclipse.emf.validation.EMFEventType;
+import org.eclipse.emf.validation.IValidationContext;
+
+public class IDValidSegmentConstraint 
+	extends AbstractModelConstraint {
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.emf.validation.AbstractModelConstraint#validate(org.eclipse.emf.validation.IValidationContext)
+	 */
+	public IStatus validate(IValidationContext ctx) {
+		EObject eObj = ctx.getTarget();
+		EMFEventType eType = ctx.getEventType();
+		
+		// In the case of batch mode.
+		if (eType == EMFEventType.NULL) {
+			//Not used model has a batch contraint
+		// In the case of live mode.
+		} else {
+			Object newValue = ctx.getFeatureNewValue();
+			
+			if (newValue == null
+				|| ((String)newValue).length() == 0) {
+				return ctx.createFailureStatus(new Object[] {eObj.eClass().getName()});
+			}
+			if(!URI.validSegment((String)newValue)){
+				return ctx.createFailureStatus(new Object[] {eObj.eClass().getName()});
+			}
+		}
+		
+		return ctx.createSuccessStatus();
+	}
+
+}
