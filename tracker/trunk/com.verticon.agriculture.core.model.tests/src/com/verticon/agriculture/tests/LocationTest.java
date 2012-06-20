ceaabbd663a10ebd7be7da6930ae683d2bcd0faf
John Conlon <jconlon@verticon.com>
2012-06-20 20:30:24 +0000

    CLOSED - # 786: Optimize Location constraints and attributes for storage https://verticon-trac.sourcerepo.com/verticon_Tracker/ticket/786

--- tracker/trunk/com.verticon.agriculture.core.model.tests/src/com/verticon/agriculture/tests/LocationTest.java
+++ tracker/trunk/com.verticon.agriculture.core.model.tests/src/com/verticon/agriculture/tests/LocationTest.java
@@ -15,13 +15,9 @@ import java.io.IOException;
 import junit.framework.TestCase;
 import junit.textui.TestRunner;
 
-import org.eclipse.emf.common.notify.NotificationChain;
 import org.eclipse.emf.ecore.EStructuralFeature;
-import org.eclipse.emf.ecore.InternalEObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.util.FeatureMap;
-import org.eclipse.emf.ecore.util.FeatureMap.Entry.Internal;
 
 import com.verticon.agriculture.AgricultureFactory;
 import com.verticon.agriculture.Location;
@@ -29,18 +25,23 @@ import com.verticon.opengis.kml.Document;
 import com.verticon.opengis.kml.Folder;
 import com.verticon.opengis.kml.KmlFactory;
 import com.verticon.opengis.kml.KmlPackage;
-import com.verticon.opengis.kml.Placemark;
 import com.verticon.tracker.Premises;
 import com.verticon.tracker.TrackerFactory;
 
 /**
- * <!-- begin-user-doc -->
- * A test case for the model object '<em><b>Location</b></em>'.
- * <!-- end-user-doc -->
+ * <!-- begin-user-doc --> A test case for the model object '
+ * <em><b>Location</b></em>'. <!-- end-user-doc -->
  * <p>
  * The following features are tested:
  * <ul>
  *   <li>{@link com.verticon.agriculture.Location#getUri() <em>Uri</em>}</li>
+ * </ul>
+ * </p>
+ * <p>
+ * The following operations are tested:
+ * <ul>
+ *   <li>{@link com.verticon.agriculture.Location#getAddress() <em>Get Address</em>}</li>
+ *   <li>{@link com.verticon.agriculture.Location#getCoordinates() <em>Get Coordinates</em>}</li>
  * </ul>
  * </p>
  * @generated
@@ -50,15 +51,14 @@ public class LocationTest extends TestCase {
 	private static final String MYURI = "myuri";
 	/**
 	 * The fixture for this Location test case.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!--
+	 * end-user-doc -->
 	 * @generated
 	 */
 	protected Location fixture = null;
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
 	 * @generated
 	 */
 	public static void main(String[] args) {
@@ -66,9 +66,9 @@ public class LocationTest extends TestCase {
 	}
 
 	/**
-	 * Constructs a new Location test case with the given name.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * Constructs a new Location test case with the given name. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	public LocationTest(String name) {
@@ -96,8 +96,7 @@ public class LocationTest extends TestCase {
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
 	 * @see junit.framework.TestCase#setUp()
 	 * @generated
 	 */
@@ -107,8 +106,7 @@ public class LocationTest extends TestCase {
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
 	 * @see junit.framework.TestCase#tearDown()
 	 * @generated
 	 */
@@ -118,91 +116,103 @@ public class LocationTest extends TestCase {
 	}
 
 	/**
-	 * Tests the '{@link com.verticon.agriculture.Location#getUri() <em>Uri</em>}' feature getter.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * Tests the '{@link com.verticon.agriculture.Location#getUri()
+	 * <em>Uri</em>}' feature getter. <!-- begin-user-doc --> <!-- end-user-doc
+	 * -->
+	 * 
 	 * @see com.verticon.agriculture.Location#getUri()
 	 * @generated NOT
 	 */
 	public void testGetUri() {
-		
+
 		Premises premises = TrackerFactory.eINSTANCE.createPremises();
 		premises.setUri(MYURI);
 		getFixture().setLivestock(premises);
 		assertNotNull((getFixture().getUri()));
 		assertEquals(MYURI, getFixture().getUri());
 	}
-	
+
+	/**
+	 * Tests the '{@link com.verticon.agriculture.Location#getAddress() <em>Get Address</em>}' operation.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see com.verticon.agriculture.Location#getAddress()
+	 * @generated
+	 */
+	public void testGetAddress() {
+		// TODO: implement this operation test method
+		// Ensure that you remove @generated or mark it @generated NOT
+		fail();
+	}
+
+	/**
+	 * Tests the '{@link com.verticon.agriculture.Location#getCoordinates() <em>Get Coordinates</em>}' operation.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see com.verticon.agriculture.Location#getCoordinates()
+	 * @generated
+	 */
+	public void testGetCoordinates() {
+		// TODO: implement this operation test method
+		// Ensure that you remove @generated or mark it @generated NOT
+		fail();
+	}
+
 	/**
 	 * Test options for Timestamps p638 model book
+	 * 
 	 * @throws IOException
 	 */
-	public void testPersistLocation() throws IOException{
+	public void testPersistLocation() throws IOException {
 		ResourceSet resourceSet = getResoureSet();
 		Premises premises = TrackerFactory.eINSTANCE.createPremises();
 		premises.setUri(MYURI);
 		getFixture().setLivestock(premises);
-//		getFixture().setId("test");
-		
-		Resource resource = createXMIResource("test.premises",resourceSet);
+
+		Resource resource = createXMIResource("test.premises", resourceSet);
 		resource.getContents().add(premises);
 		resource.save(null);
-		
-		
-	    resource = createXMIResource("test.agri",resourceSet);
+
+		resource = createXMIResource("test.agri", resourceSet);
 		resource.getContents().add(getFixture());
 		resource.save(null);
-	
+
 	}
-	
-	public void testValidation(){
+
+	public void testValidation() {
 		assertFalse(isValidObject(getFixture()));
 		Premises premises = TrackerFactory.eINSTANCE.createPremises();
 		getFixture().setLivestock(premises);
 		assertFalse(isValidObject(getFixture()));
 		premises.setUri("test");
-//		getFixture().setId("test");
-		assertFalse(isValidObject(getFixture()));//Still needs geography
-		
-		//Create a KML Document
+
+		assertFalse(isValidObject(getFixture()));// Still needs geography
+
+		// Create a KML Document
 		Document document = KmlFactory.eINSTANCE.createDocument();
 		document.setDescription("Doc level");
 		getFixture().setGeography(document);
-		assertFalse(isValidObject(getFixture()));//Still needs placemark
-		
-		
-		//Add a Folder to the Document
+		assertFalse(isValidObject(getFixture()));// Still needs placemark
+
+		// Add a Folder to the Document
 		Folder folder = KmlFactory.eINSTANCE.createFolder();
 		folder.setName("MyFolder");
 		folder.setDescription("The first folder");
 		EStructuralFeature f = KmlPackage.eINSTANCE.getDocumentRoot_Folder();
-	    document.getAbstractFeatureGroupGroup().add(f,folder);
-				
-		//Add a Placemark to the folder
-		Placemark placemark =  KmlFactory.eINSTANCE.createPlacemark();
-		placemark.setName("MyPlacemark");
-		placemark.setId("12345");
-		f = KmlPackage.eINSTANCE.getDocumentRoot_Placemark();
-		folder.getAbstractFeatureGroupGroup().add(f,placemark);
-		
-		
-		getFixture().setPlace(placemark);
-		assertTrue(isValidObject(getFixture()));//Still needs placemark
-		
-//		//Change the id of the location
-//		getFixture().setId("ggg://###");
-//		assertFalse(isValidObject(getFixture()));//Bad id
-//		//Fix it
-//		getFixture().setId("ggg:");
-//		assertTrue(isValidObject(getFixture()));
-		
+		document.getAbstractFeatureGroupGroup().add(f, folder);
+		assertFalse(isValidObject(getFixture()));// Still needs name...
+		getFixture().setName("test");
+		getFixture().setDescription("test");
+		getFixture().setStreet("test");
+		getFixture().setCity("test");
+		getFixture().setState("test");
+		getFixture().setPostalCode("test");
+		getFixture().setLongitude(111);
+		getFixture().setLatitude(-111);
+		getFixture().setAltitude(999);
+		getFixture().setPhoneNumber("test");
+		assertTrue(isValidObject(getFixture()));
+
 	}
-	
-	
-	
-	 
-	
-	
 
-
-} //LocationTest
+} // LocationTest
