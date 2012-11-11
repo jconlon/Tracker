f6c8c164351f4bcb54949592e0622a08d9589be6
John Conlon <jconlon@verticon.com>
2012-11-11 04:35:15 +0000

    NEW - # 815: Add version constraints to changed plugins. https://verticon-trac.sourcerepo.com/verticon_Tracker/ticket/815

--- tracker/trunk/com.verticon.agriculture.core.model.tests/src/com/verticon/agriculture/tests/AssociationTest.java
+++ tracker/trunk/com.verticon.agriculture.core.model.tests/src/com/verticon/agriculture/tests/AssociationTest.java
@@ -6,14 +6,20 @@
  */
 package com.verticon.agriculture.tests;
 
-import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
 import junit.framework.TestCase;
 import junit.textui.TestRunner;
 
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+
 import com.verticon.agriculture.AgricultureFactory;
+import com.verticon.agriculture.AgriculturePackage;
 import com.verticon.agriculture.Association;
+import com.verticon.agriculture.util.AgricultureResourceFactoryImpl;
 
 /**
  * <!-- begin-user-doc -->
@@ -95,6 +101,38 @@ public class AssociationTest extends TestCase {
 	
 	public void testInstance(){
 	    assertThat("Association must not be null", getFixture(), is(notNullValue()));
+	}
+
+	public void testAgriculturePackageInstantiation() {
+
+		AgriculturePackage agrpac = AgriculturePackage.eINSTANCE;
+		assertThat("AgriculturePackage must not be null", agrpac,
+				is(notNullValue()));
+	}
+	public void testAgriculturePackageRegistration() {
+
+		// }
+		//
+		// private Resource load() {
+		// // Read the file
+		// // Create a resource set to hold the resources.
+		ResourceSet resourceSet = new ResourceSetImpl();
+		// Register the appropriate resource factory to handle all file
+		// extensions.
+		resourceSet
+				.getResourceFactoryRegistry()
+				.getExtensionToFactoryMap()
+				.put(Resource.Factory.Registry.DEFAULT_EXTENSION,
+						new AgricultureResourceFactoryImpl());
+		// Register the package to ensure it is available during loading.
+		resourceSet.getPackageRegistry().put(AgriculturePackage.eNS_URI,
+				AgriculturePackage.eINSTANCE);
+
+		// URI uri = createURI(getProject());
+		// logger.debug(bundleMarker, "{} loading {}", this, uri);
+		//
+		// return resourceSet.getResource(uri, true);
+
 	}
 
 } //AssociationTest
