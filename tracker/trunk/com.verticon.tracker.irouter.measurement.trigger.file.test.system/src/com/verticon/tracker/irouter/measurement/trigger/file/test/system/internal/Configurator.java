3595096261a7bd9c22313d7706952adaacb064ca
John Conlon <jconlon@verticon.com>
2013-03-01 22:01:39 +0000

    CLOSED - # 853: Add eventAdmin gateway to unit testing https://verticon-trac.sourcerepo.com/verticon_Tracker/ticket/853

--- tracker/trunk/com.verticon.tracker.irouter.measurement.trigger.file.test.system/src/com/verticon/tracker/irouter/measurement/trigger/file/test/system/internal/Configurator.java
+++ tracker/trunk/com.verticon.tracker.irouter.measurement.trigger.file.test.system/src/com/verticon/tracker/irouter/measurement/trigger/file/test/system/internal/Configurator.java
@@ -28,11 +28,16 @@ import org.slf4j.Marker;
 import org.slf4j.MarkerFactory;
 
 /**
+ * 
+ * Configures a com.verticon.tracker.irouter.measurement.trigger.file and a
+ * com.verticon.tracker.irouter.eventadmin.gateway
+ * 
  * @author jconlon
  * 
  */
 public class Configurator {
 
+	private static final String EVENTADMIN_GATEWAY = "com.verticon.tracker.irouter.eventadmin.gateway";
 
 	static final String OHAUS_WEIGHT = "ohaus.weight";
 	static String PLUGIN_ID = "com.verticon.tracker.irouter.measurement.trigger.file.test.system";
@@ -53,20 +58,33 @@ public class Configurator {
     
 
 
+	/**
+	 * Configuration Admin is injected here. It will be used to configure a
+	 * 
+	 * @param configAdmin
+	 * @throws IOException
+	 */
 	public void setConfigurationAdmin(ConfigurationAdmin configAdmin)
 			throws IOException {
 		logger.debug(bundleMarker, "Setting ConfigurationAdmin");
 		deleteConfigurations(configAdmin);
 		
-		// Configure the first test class
+		// Configure the Measurement Trigger File
 		Configuration config = configAdmin
 				.createFactoryConfiguration(FACTORY_PID.getKey());
-		Dictionary<String, Object> props = configureTest1();
+		Dictionary<String, Object> props = configureMeasurementTriggerFile();
 		config.update(props);
-		logger.debug(bundleMarker, "Created Configuration for Test 1 {}",
+		logger.debug(bundleMarker,
+				"Created Configuration for measurement.trigger.file {}",
 				config.getPid());
 
-		// Configure the Measurement Trigger File
+		// Configure the Event Admin Gateway
+		config = configAdmin.createFactoryConfiguration(EVENTADMIN_GATEWAY);
+		props = configureEventAdminGateway();
+		config.update(props);
+		logger.debug(bundleMarker,
+				"Created Configuration for event.admin.gateway {}",
+				config.getPid());
 
 	}
 
@@ -93,7 +111,7 @@ public class Configurator {
 		}
 	}
 
-	private static Dictionary<String, Object> configureTest1() {
+	private static Dictionary<String, Object> configureMeasurementTriggerFile() {
 		Dictionary<String, Object> config = new Hashtable<String, Object>();
 		config.put(
 				"com.verticon.tracker.irouter.measurement.trigger.file.test",
@@ -102,7 +120,21 @@ public class Configurator {
 		CONNECTION_URI.configure(config, "file:///tmp");
 
 		config.put(WireConstants.WIREADMIN_CONSUMER_SCOPE,
-				new String[] { OHAUS_WEIGHT });
+ new String[] {
+				OHAUS_WEIGHT, "event.admin.gateway" });
+		return config;
+	}
+
+	private static Dictionary<String, Object> configureEventAdminGateway() {
+		Dictionary<String, Object> config = new Hashtable<String, Object>();
+		config.put("com.verticon.tracker.irouter.eventadmin.gateway.test",
+				"testOne");
+		WIRING_GROUP.configure(config, "test");// constant doesnt matter
+		// TOPIC
+		config.put("event.topics", "com/verticon/tracker/irouter");
+
+		config.put(WireConstants.WIREADMIN_PRODUCER_SCOPE,
+				new String[] { "event.admin.gateway" });
 		return config;
 	}
 }
