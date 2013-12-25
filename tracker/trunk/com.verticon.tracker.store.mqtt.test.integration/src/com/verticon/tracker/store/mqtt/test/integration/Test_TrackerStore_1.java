a499c2844240e678f8ab202b5c285c804ebc9402
jconlon <jconlon@0cc8e0f8-e01b-0410-8bcf-b9e3f263e42e>
2013-12-25 01:04:45 +0000

    CLOSED - # 932: Refactor system and integration tests for MongoDB and MQTT https://verticon-trac.sourcerepo.com/verticon_Tracker/ticket/932

--- tracker/trunk/com.verticon.tracker.store.mqtt.test.integration/src/com/verticon/tracker/store/mqtt/test/integration/Test_TrackerStore_1.java
+++ tracker/trunk/com.verticon.tracker.store.mqtt.test.integration/src/com/verticon/tracker/store/mqtt/test/integration/Test_TrackerStore_1.java
@@ -12,8 +12,6 @@
 
 package com.verticon.tracker.store.mqtt.test.integration;
 
-
-import static com.google.common.collect.Sets.newHashSet;
 import static com.verticon.tracker.store.mqtt.test.integration.Configurator.PREMISES_URI_H89234X;
 import static com.verticon.tracker.store.mqtt.test.integration.Configurator.bundleMarker;
 import static com.verticon.tracker.store.mqtt.test.integration.Configurator.getXMIResource;
@@ -24,15 +22,11 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
 import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.TimeUnit;
@@ -40,7 +34,6 @@ import java.util.concurrent.TimeoutException;
 
 import junit.framework.TestCase;
 
-import org.bson.BSONObject;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.impl.BinaryResourceImpl;
@@ -50,14 +43,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.collect.Iterables;
-import com.mongodb.util.JSON;
-import com.verticon.tracker.Animal;
-import com.verticon.tracker.AnimalType;
+import com.verticon.mqtt.test.utilities.ISubscriberListener;
+import com.verticon.mqtt.test.utilities.ISubscriberPublisher;
 import com.verticon.tracker.Premises;
-import com.verticon.tracker.store.ITrackerFind;
 import com.verticon.tracker.store.ITrackerStore;
 import com.verticon.tracker.store.IUpdateStats;
-import com.verticon.tracker.store.Query;
 
 /**
  * 
@@ -82,17 +72,10 @@ import com.verticon.tracker.store.Query;
 public class Test_TrackerStore_1 extends TestCase implements
 		ISubscriberListener {
 
-	private final static DateFormat DATE_FORMAT = new SimpleDateFormat(
-			ITrackerFind.DATE_PATTERN);
-
-	private static final int NUMBER_OF_ITERATIONS = 100;
-
-	private static final String DATE_START = "2011-01-01";
-	private static final String DATE_2011_05_01 = "2011-05-01";
-
 	static final String DOC_PREMISES = "example.premises";
-	private static final String TAG_ID_2 = "abcd";
 	static final String TAG_ID_1 = "1234567890";
+
+	private static final Map<String, MqttMessage> messages = new ConcurrentHashMap<String, MqttMessage>();
 
 	/**
 	 * slf4j Logger
@@ -124,7 +107,7 @@ public class Test_TrackerStore_1 extends TestCase implements
 	 * Injected services
 	 */
 	static ITrackerStore trackerStore;
-	static ISubscriber subscriber;
+	static ISubscriberPublisher subscriber;
 
 	/**
 	 * Injected by ds
@@ -144,12 +127,12 @@ public class Test_TrackerStore_1 extends TestCase implements
 		Test_TrackerStore_1.trackerStore = null;
 	}
 
-	void setSubscriber(ISubscriber subscriber) {
+	void setSubscriber(ISubscriberPublisher subscriber) {
 		Test_TrackerStore_1.subscriber = subscriber;
 		subscriber.setListener(this);
 	}
 
-	void unsetSubscriber(ISubscriber subscriber) {
+	void unsetSubscriber(ISubscriberPublisher subscriber) {
 		logger.debug(bundleMarker, "DS removing the subscriber");
 		subscriber.setListener(null);
 		Test_TrackerStore_1.subscriber = null;
@@ -169,6 +152,7 @@ public class Test_TrackerStore_1 extends TestCase implements
 		super.setUp();
 		startUpGate.await();// wait for startUp to finish
 		logger.info(bundleMarker, "START TEST");
+		messageReceivedBarrier.reset();
 	}
 
 	/*
@@ -203,6 +187,7 @@ public class Test_TrackerStore_1 extends TestCase implements
 		// is("tcp://m2m.eclipse.org:1883"));
 				is("tcp://tuna:1883"));
 	}
+
 	/**
 	 * Test authorization and set it
 	 * 
@@ -228,9 +213,9 @@ public class Test_TrackerStore_1 extends TestCase implements
 			fail("Timed out wait for message.");
 		}
 
-		assertThat("Must have one message", subscriber.messages().size(), is(1));
-		Entry<String, MqttMessage> messageEntry = Iterables.get(subscriber
-				.messages().entrySet(), 0);
+		assertThat("Must have one message", messages.size(), is(1));
+		Entry<String, MqttMessage> messageEntry = Iterables.get(
+				messages.entrySet(), 0);
 		String topicName = messageEntry.getKey();
 		MqttMessage message = messageEntry.getValue();
 		byte[] payload = message.getPayload();
@@ -241,39 +226,6 @@ public class Test_TrackerStore_1 extends TestCase implements
 
 		assertThat("Topic name is wrong", topicName,
 				is("Agriculture/Premises/urn:pin:H89234X/Register/EMF"));
-	}
-
-	@Test
-	public void test_TrackerFind_RetrievePremises() throws IOException {
-
-		Premises premises = trackerStore.retrievePremises(PREMISES_URI_H89234X);
-
-		assertThat("Premises should not be null", premises, is(notNullValue()));
-		assertThat("Premises should have name", premises.getName(),
-				is("East Farm"));
-		assertThat("Premises should have no Animals", premises.getAnimals()
-				.size(), is(0));
-		assertThat("Premises should have no unassignedTags", premises
-				.getUnAppliedTags().size(), is(0));
-		assertThat("URI must be urn:pin:H89234X", premises.getUri(),
-				is(PREMISES_URI_H89234X));
-		assertThat("Should have five areas", premises.getLocation().getAreas()
-				.size(), is(5));
-	}
-
-
-
-	@Test
-	public void test_QueryForLastPub() throws IOException {
-		String result = trackerStore.query(Query.LAST_PUB_QUERY_TEMPLATE
-				.replace("12345"));
-		assertThat("Result must not be null.", result, is(notNullValue()));
-		BSONObject response = (BSONObject) JSON.parse(result);
-		logger.debug(bundleMarker, "Response is: {}", result);
-		List<?> list = (List<?>) response.get("result");
-		assertThat("Result list must be one", list.size(), is(1));
-		assertThat("Wrong type", ((BSONObject) list.get(0)).get("update"),
-				is(instanceOf(Date.class)));
 	}
 
 	@Test
@@ -304,113 +256,49 @@ public class Test_TrackerStore_1 extends TestCase implements
 				is(premises.eventHistory().size()));
 	}
 
-	@Test
-	public void test_RetrieveAnimal() throws IOException, InterruptedException,
-			BrokenBarrierException {
-		Animal animal = trackerStore.retrieveAnimal("840456789012341");
-
-		assertThat("Animal should not be null", animal, is(notNullValue()));
-
-		assertThat("Animal should be a swine", animal.getType(),
-				is(AnimalType.BOVINE_BEEF));
-
-		assertThat("Animal should have 1 tag", animal.getTags().size(), is(1));
-		assertThat("Animal should have id", animal.getId(),
-				is("840456789012341"));
-		assertThat("Animal should have 8 events", animal.eventHistory().size(),
-				is(8));
-
-	}
-
-	@Test
-	public void test_TrackerFind_RetrievePremises_Dates_Simple()
-			throws IOException {
-
-		Calendar c = Calendar.getInstance();
-		c.add(Calendar.DAY_OF_MONTH, 1);
-		Premises premises = trackerStore.retrievePremises(PREMISES_URI_H89234X,
-				DATE_START, DATE_FORMAT.format(c.getTime()));
-		assertThat("Premises should not be null", premises, is(notNullValue()));
-
-		assertThat("Premises should have name", premises.getName(),
-				is("East Farm"));
-		assertThat("Premises should have 3 Animals", premises.getAnimals()
-				.size(), is(3));
-		// Animal animal = premises.getAnimals().get(0);
-
-		assertThat("Premises should have 3 events", premises.eventHistory()
-				.size(), is(10));
-
-	}
-
-	/**
-	 * db.Premises.find( { "location.loc" : { $near :
-	 * [-90.95048182270057,43.47622307339506,0 ] , $maxDistance : 1 } } )
-	 * 
-	 * @throws IOException
-	 */
-	@Test
-	public void test_TrackerFind_RetrievePremises_Point() throws IOException {
-		ITrackerFind.LongLatPoint point = new ITrackerFind.LongLatPoint(
-				"-77.037852,38.898556,0");
-		// position.setLatitude(38.898556);
-		// position.setLongitude(-77.037852);
-		Premises premises = trackerStore.retrievePremises(point);
-
-		assertThat("Premises should not be null", premises, is(notNullValue()));
-
-		assertThat("Premises should have uri", premises.getUri(),
-				is(PREMISES_URI_H89234X));
-		assertThat("Premises should have name", premises.getName(),
-				is("East Farm"));
-		assertThat("Premises should have no Animals", premises.getAnimals()
-				.size(), is(0));
-		assertThat("Premises should have no unassignedTags", premises
-				.getUnAppliedTags().size(), is(0));
-	}
-
-	@Test
-	public void test_TrackerFind_RetrievePremises_Point_Dates()
-			throws IOException {
-		ITrackerFind.LongLatPoint point = new ITrackerFind.LongLatPoint(
-				"-90.95048182270057,43.47622307339506,0");
-		Premises premises = trackerStore.retrievePremises(point, DATE_START,
-				DATE_2011_05_01);
-
-		assertThat("Premises should not be null", premises, is(notNullValue()));
-		assertThat("Premises should have uri", premises.getUri(),
-				is(PREMISES_URI_H89234X));
-		assertThat("Premises should have name", premises.getName(),
-				is("East Farm"));
-		assertThat("Premises should have 1 Animals", premises.getAnimals()
-				.size(), is(1));
-		assertThat("Premises should have no unassignedTags", premises
-				.getUnAppliedTags().size(), is(0));
-	}
-
-	@Test
-	public void test_getPremisesNames() throws InterruptedException {
-		Set<String> uris = newHashSet(PREMISES_URI_H89234X, "xyz", "abc");
-		Map<String, String> nameMap = trackerStore.getPremisesNames(uris);
-		assertThat("Should not be null", nameMap, is(notNullValue()));
-		assertThat("Should have 1 entries", nameMap.size(), is(1));
-		// System.out.println("******" + nameMap);
-		// String name = nameMap.get(PREMISES_URI);
-		// assertThat("Should not be null", name, is(notNullValue()));
-		// assertThat("Should be", name, is("Jack Condor"));
-
-		String name = nameMap.get(PREMISES_URI_H89234X);
-		assertThat("Should not be null", name, is(notNullValue()));
-		assertThat("Should be", name, is("East Farm"));
-	}
-
 	@Override
-	public void messageArrived() {
-		logger.debug(bundleMarker, "Message arrived in testcase");
+	public void messageArrived(String topic, MqttMessage message) {
+		if (topic.endsWith("Query")) {
+			handleQuery(topic, message);
+		} else if (topic.endsWith("Response")) {
+			logger.info(bundleMarker, "Ignoring topic {}", topic);
+
+		} else {
+			logger.info(bundleMarker, "Message arrived on topic {}", topic);
+			try {
+				messages.put(topic, message);
+				messageReceivedBarrier.await();
+			} catch (Exception e) {
+				logger.error(bundleMarker, "Barrier problem.", e);
+			}
+		}
+
+	}
+
+	@SuppressWarnings("unused")
+	private void handleQuery(String topic, MqttMessage message) {
+		logger.info(bundleMarker, "Query arrived on topic {}", topic);
+		String query = new String(message.getPayload());
+		byte[] payload;
 		try {
-			messageReceivedBarrier.await();
+			if (query.startsWith("{trackerStore")) {
+				// payload = handleTrackerStoreQuery(topic, query);
+			} else if (query.startsWith("{aggregate")) {
+				logger.debug(bundleMarker, "Received update query: " + query);
+				// String response = "{'result' : [{"
+				// + "'update' : '2012-10-08T22:36:39.046Z'"//
+				// ISODate('2012-10-08T22:36:39.046Z')"
+				// + "}],'ok' : 1}";
+				// payload = response.getBytes();
+			} else {
+				logger.debug(bundleMarker, "Unknown query {}", query);
+				return;
+			}
+			// String responseTopic = topic.replace("Query", "Response");
+			// client.publish(responseTopic, payload, 2, false);
 		} catch (Exception e) {
-			logger.error(bundleMarker, "Barrier problem.", e);
+			logger.error(bundleMarker, "Failed to handle query " + query, e);
+
 		}
 
 	}
